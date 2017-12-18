4ce233fc7703b54272a02525eef19720a8de211d
Primiano Tucci <primiano@google.com>
2017-12-18 11:20:11 +0100

    [Patchset 1] Add IdAllocator class
    It will be used by both SharedMemoryArbiter and tracing::ServiceImpl
    to allocate IDs for, respectively, writer IDs and buffer IDs.
    
    Test: perfetto_tests --gtest_filter=IdAllocatorTest.*
    Bug: 70284518
    Change-Id: Id7cb75fbc256f04515ef24531f8a23cffbfad781

---
+++ src/tracing/core/id_allocator.cc
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/tracing/core/id_allocator.h"
+
+#include "perfetto/base/logging.h"
+
+namespace perfetto {
+
+IdAllocator::IdAllocator(IdType max) : max_(max) {}
+IdAllocator::~IdAllocator() = default;
+
+IdAllocator::IdType IdAllocator::Allocate() {
+  for (IdType i = 0; i < max_; i++) {
+    last_id_ = last_id_ < max_ - 1 ? last_id_ + 1 : 1;
+    const auto id = last_id_;
+
+    // 0 is never a valid ID. So if we are looking for |id| == N and there are
+    // N or less elements in the vector, they must necessarily be all < N.
+    // e.g. if |id| == 4 and size() == 4, the vector will contain IDs 0,1,2,3.
+    if (id >= ids_.size()) {
+      ids_.resize(id + 1);
+      ids_[id] = true;
+      return id;
+    }
+
+    if (!ids_[id]) {
+      ids_[id] = true;
+      return id;
+    }
+  }
+  return 0;
+}
+
+void IdAllocator::Free(IdType id) {
+  if (id >= ids_.size() || !ids_[id]) {
+    PERFETTO_DCHECK(false);
+    return;
+  }
+  ids_[id] = false;
+}
+
+}  // namespace perfetto
