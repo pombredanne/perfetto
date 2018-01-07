4e89f4183225ea23592330df40a718bf9ff5effb
Oystein Eftevaag <oysteine@google.com>
2018-01-07 14:16:04 -0800

    [Patchset 1] Added targets for linking Perfetto as an embedded library and fixed some include paths
    Bug:
    Change-Id: I448fe7c110be4f799dcbfd5604da9e94921eede3

--- include/perfetto/tracing/core/trace_config.h
+++ include/perfetto/tracing/core/trace_config.h
@@ -32,9 +32,8 @@
 #include <string>
 #include <type_traits>
 #include <vector>
-#include "perfetto/base/build_config.h"
 
-#include "include/perfetto/tracing/core/data_source_config.h"
+#include "perfetto/tracing/core/data_source_config.h"
 
 // Forward declarations for protobuf types.
 namespace perfetto {
