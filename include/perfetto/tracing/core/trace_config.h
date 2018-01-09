cf39c7a53c08d31cca1d7be50bbbc2b9040ced46
Oystein Eftevaag <oysteine@google.com>
2018-01-09 11:41:58 -0800

    [Patchset 8] Added targets for linking Perfetto as an embedded library and fixed some include paths
    Bug:
    Change-Id: I448fe7c110be4f799dcbfd5604da9e94921eede3

--- include/perfetto/tracing/core/trace_config.h
+++ include/perfetto/tracing/core/trace_config.h
@@ -42,6 +42,7 @@ class TraceConfig;
 class TraceConfig_BufferConfig;
 class TraceConfig_DataSource;
 class DataSourceConfig;
+class DataSourceConfig_FtraceConfig;
 }  // namespace protos
 }  // namespace perfetto
 
