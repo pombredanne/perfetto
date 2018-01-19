13193e054522b531b37512b27826d698f974bd92
Hector Dearman <hjd@google.com>
2018-01-19 12:22:42 +0000

    [Patchset 2] add experiment
    enable meta tracing
    
    
    add plumb kernerl size
    
    Bug:
    Change-Id: I7e0259b1573e0b704e44f502a93cb9b1913ad589

--- experiment.sh
+++ experiment.sh
@@ -4,6 +4,10 @@ set -e
 
 OUT_DIR=out/linux_clang_debug/
 
+DURATION_MS="${DURATION_MS:-10000}"
+BUFFER_SIZE_KB="${BUFFER_SIZE_KB:-}"
+DRAIN_PERIOD_MS="${DRAIN_PERIOD_MS:-}"
+
 function make_config() {
   local buffer_size_kb=$1
   local drain_period_ms=$2
@@ -16,8 +20,38 @@ function make_config() {
   $OUT_DIR/protoc_helper encode
 }
 
-make_config 1024 100 5000 > experiment.cfg.protobuf
-adb push experiment.cfg.protobuf /data/local/
+if [[ -z "$DURATION_MS" ]]; then
+  echo "Please set DURATION_MS"
+  exit 1
+fi
+
+if [[ -z "$BUFFER_SIZE_KB" ]]; then
+  echo "Please set BUFFER_SIZE_KB"
+  exit 1
+fi
+
+if [[ -z "$DRAIN_PERIOD_MS" ]]; then
+  echo "Please set DRAIN_PERIOD_MS"
+  exit 1
+fi
+
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 output_trace_name.html"
+  exit 1
+fi
+
+output=$1
+
+adb shell setenforce 0
+
+tmpdir=$(mktemp -d)
+trap 'rm -rf -- "$tmpdir"' INT TERM HUP EXIT
+
+metadatadir=$output.metadata
+mkdir $metadatadir
+
+make_config $BUFFER_SIZE_KB $DRAIN_PERIOD_MS $DURATION_MS> $tmpdir/experiment.cfg.protobuf
+adb push $tmpdir/experiment.cfg.protobuf /data/local/
 
 set -x
 
@@ -26,12 +60,19 @@ adb shell mkdir -p /sys/kernel/debug/tracing/instances/meta
 adb shell stop traced
 adb shell stop traced_probes
 
-adb shell start traced
-adb shell start traced_probes
+adb shell traced &
+adb shell traced_probes &
 
-#adb shell perfetto -c /data/local/experiment.cfg.protobuf -o /data/local/out.protobuf &
-./buildtools/android_sdk/platform-tools/systrace/systrace.py -t 30 -o out.html sched 
+adb shell perfetto -c /data/local/experiment.cfg.protobuf -o /data/local/out.protobuf &
+./buildtools/android_sdk/platform-tools/systrace/systrace.py -t 10 -o $output sched
 
-adb shell stop traced
-adb shell stop traced_probes
+set +x
+
+for i in {0..7}; do
+  adb pull /sys/kernel/debug/tracing/instances/meta/per_cpu/cpu$i/stats $metadatadir/stats$i
+done
+
+find $metadatadir/ -type f | xargs grep '^overrun:' | cut -d' ' -f2 | paste -sd+ - | bc > $metadatadir/overrun
+echo 'dropped:' $(cat $metadatadir/overrun)
+
 
