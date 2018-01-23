5a2fc1a93ba0b677694eb70d1c6dc935c1064190
Florian Mayer <fmayer@google.com>
2018-01-23 15:00:01 +0000

    [Patchset 4] Run clang-tidy
    Change-Id: I144335b7cd196e453351db3a83a214fb2f0245da

--- src/tracing/test/hello_world_benchmark.cc
+++ src/tracing/test/hello_world_benchmark.cc
@@ -3,7 +3,7 @@
 static void BM_StringCreation(benchmark::State& state) {
   while (state.KeepRunning()) {
     std::string empty_string;
-}
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_StringCreation);
@@ -12,7 +12,7 @@ BENCHMARK(BM_StringCreation);
 static void BM_StringCopy(benchmark::State& state) {
   std::string x = "hello";
   while (state.KeepRunning()) {
-    const std::string& copy(x);
+    std::string copy(x);  // NOLINT
   }
 }
 BENCHMARK(BM_StringCopy);
