124e516341aad31086635f6d4899fe7d1143e647
Hector Dearman <hjd@google.com>
2018-01-24 14:47:59 +0000

    [Patchset 1] ftrace_reader: Add first benchmark
    Change-Id: Ia03df7d9453c098ba3d20d1fc92a95dfc42d678b

--- include/perfetto/tracing/core/trace_config.h
+++ include/perfetto/tracing/core/trace_config.h
@@ -33,6 +33,7 @@
 #include <type_traits>
 #include <vector>
 
+#include "perfetto/base/build_config.h"
 #include "perfetto/tracing/core/data_source_config.h"
 
 // Forward declarations for protobuf types.
