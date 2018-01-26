1f269951210fabbf60ded3ae2cc8728ad4d9400d
Primiano Tucci <primiano@google.com>
2018-01-26 14:54:40 +0000

    [Patchset 1] DO NOT MERGE: Proof of concept for Dropbox leak
    STOPSHIP, this is just a POC
    
    Repro steps:
    m dropbox_leak
    adb push && adb shell
    look at /proc/PID/fd:
    
    walleye:/ # ls -l /proc/20093/fd/
    total 0
    lrwx------ 1 root root 64 2018-01-26 14:50 0 -> /dev/pts/0
    lrwx------ 1 root root 64 2018-01-26 14:50 1 -> /dev/pts/0
    lrwx------ 1 root root 64 2018-01-26 14:50 10 -> socket:[162503]
    lrwx------ 1 root root 64 2018-01-26 14:50 100 -> /data/local/tmp/tmp.90 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 101 -> /data/local/tmp/tmp.91 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 102 -> /data/local/tmp/tmp.92 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 103 -> /data/local/tmp/tmp.93 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 104 -> /data/local/tmp/tmp.94 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 105 -> /data/local/tmp/tmp.95 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 106 -> /data/local/tmp/tmp.96 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 107 -> /data/local/tmp/tmp.97 (deleted)
    lrwx------ 1 root root 64 2018-01-26 14:50 108 -> /data/local/tmp/tmp.98 (deleted
    Change-Id: Ia94196705f0b5e8e945682877db907f8b073639c

---
+++ dropbox_leak.cc
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+#include "perfetto/base/android_task_runner.h"
+
+#include <android/os/DropBoxManager.h>
+#include <utils/Looper.h>
+#include <utils/StrongPointer.h>
+
+
+int main() {
+  ::perfetto::base::AndroidTaskRunner task_runner;
+
+  printf("Hello, PID=%d\n", getpid());
+  for (int i = 0; i < 100; i++) {
+    char path[255];
+    sprintf(path, "/data/local/tmp/tmp.%d", i);
+    printf("Trying path %s", path);
+    int fd = open(path, O_RDWR | O_CLOEXEC | O_CREAT, 0666);
+    PERFETTO_CHECK(fd >= 0);
+    write(fd, "foo\n", 4);
+
+    android::sp<android::os::DropBoxManager> dropbox =
+        new android::os::DropBoxManager();
+    android::binder::Status status = dropbox->addFile(
+        android::String16("leaktest"), path, 0 /* flags */);
+    unlink(path);
+    printf("Binder result %d: %d", i, status.isOk());
+  }
+  task_runner.Run();
+}
