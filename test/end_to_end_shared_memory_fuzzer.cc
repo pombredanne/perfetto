5ee0a0cc583a81feb812624553d8368829afb786
Florian Mayer <fmayer@google.com>
2018-01-26 12:06:01 +0000

    [Patchset 18] Add end-to-end fuzzer.
    Change-Id: I87cdc139acc7f93fd5e86c61a6b54817fe22a8d9
    Bug: 69150303

--- test/end_to_end_shared_memory_fuzzer.cc
+++ test/end_to_end_shared_memory_fuzzer.cc
@@ -74,6 +74,9 @@ class FakeProducer : public perfetto::Producer {
     end_packet->set_test("end");
     end_packet->Finalize();
 
+    // Temporarily create a new packet to flush the final packet to the
+    // consumer.
+    // TODO(primiano): remove this hack once flushing the final packet is fixed.
     trace_writer->NewTracePacket();
   }
 
