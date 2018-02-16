205c9633d8642c569f88f6ab3a92e16476b6b8c7
Anna Zappone <azappone@google.com>
2018-02-16 12:21:36 +0000

    [Patchset 7] Save inode numbers in cpu_reader to get inode to filename mappings
    Change-Id: Ic6b38010871fb1b6f126f0176105528dbc939a3f

--- src/ftrace_reader/cpu_reader.h
+++ src/ftrace_reader/cpu_reader.h
@@ -23,9 +23,11 @@
 #include <array>
 #include <memory>
 #include <set>
+#include <thread>
 
 #include "gtest/gtest_prod.h"
 #include "perfetto/base/scoped_file.h"
+#include "perfetto/base/thread_checker.h"
 #include "perfetto/ftrace_reader/ftrace_controller.h"
 #include "perfetto/protozero/protozero_message.h"
 #include "proto_translation_table.h"
@@ -65,11 +67,19 @@ class EventFilter {
   std::set<std::string> enabled_names_;
 };
 
+// Processes raw ftrace data for a logical CPU core.
 class CpuReader {
  public:
-  CpuReader(const ProtoTranslationTable*, size_t cpu, base::ScopedFile fd);
+  // |on_data_available| will be called on an arbitrary thread when at least one
+  // page of ftrace data is available for draining on this CPU.
+  CpuReader(const ProtoTranslationTable*,
+            size_t cpu,
+            base::ScopedFile fd,
+            std::function<void()> on_data_available);
   ~CpuReader();
 
+  // Drains all available data from the staging pipe into the given sinks.
+  // Should be called in response to the |on_data_available| callback.
   bool Drain(
       const std::array<const EventFilter*, kMaxSinks>&,
       const std::array<
@@ -131,14 +141,23 @@ class CpuReader {
                          std::set<uint64_t>& inode_numbers);
 
  private:
+  static void RunWorkerThread(size_t cpu,
+                              int trace_fd,
+                              int staging_write_fd,
+                              std::function<void()> on_data_available);
+
   uint8_t* GetBuffer();
   CpuReader(const CpuReader&) = delete;
   CpuReader& operator=(const CpuReader&) = delete;
 
   const ProtoTranslationTable* table_;
   const size_t cpu_;
-  base::ScopedFile fd_;
+  base::ScopedFile trace_fd_;
+  base::ScopedFile staging_read_fd_;
+  base::ScopedFile staging_write_fd_;
   std::unique_ptr<uint8_t[]> buffer_;
+  std::thread worker_thread_;
+  PERFETTO_THREAD_CHECKER(thread_checker_)
 };
 
 }  // namespace perfetto
--- test/end_to_end_shared_memory_fuzzer.cc
+++ test/end_to_end_shared_memory_fuzzer.cc
@@ -22,6 +22,7 @@
 #include "perfetto/base/task_runner.h"
 #include "perfetto/base/utils.h"
 #include "perfetto/ipc/host.h"
+#include "perfetto/trace/test_event.pbzero.h"
 #include "perfetto/trace/trace_packet.pb.h"
 #include "perfetto/trace/trace_packet.pbzero.h"
 #include "perfetto/tracing/core/data_source_config.h"
@@ -45,8 +46,11 @@ namespace shm_fuzz {
 // consumer.
 class FakeProducer : public Producer {
  public:
-  FakeProducer(std::string name, const uint8_t* data, size_t size)
-      : name_(std::move(name)), data_(data), size_(size) {}
+  FakeProducer(std::string name,
+               const uint8_t* data,
+               size_t size,
+               FakeConsumer* consumer)
+      : name_(std::move(name)), data_(data), size_(size), consumer_(consumer) {}
 
   void Connect(const char* socket_name, base::TaskRunner* task_runner) {
     endpoint_ = ProducerIPCClient::Connect(socket_name, this, task_runner);
@@ -72,8 +76,9 @@ class FakeProducer : public Producer {
     packet->Finalize();
 
     auto end_packet = trace_writer->NewTracePacket();
-    end_packet->set_test("end");
+    end_packet->set_for_testing()->set_str("end");
     end_packet->Finalize();
+    consumer_->BusyWaitReadBuffers();
   }
 
   void TearDownDataSourceInstance(DataSourceInstanceID) override {}
@@ -84,17 +89,18 @@ class FakeProducer : public Producer {
   const size_t size_;
   DataSourceID id_ = 0;
   std::unique_ptr<Service::ProducerEndpoint> endpoint_;
+  FakeConsumer* consumer_;
 };
 
 class FakeProducerDelegate : public ThreadDelegate {
  public:
-  FakeProducerDelegate(const uint8_t* data, size_t size)
-      : data_(data), size_(size) {}
+  FakeProducerDelegate(const uint8_t* data, size_t size, FakeConsumer* consumer)
+      : data_(data), size_(size), consumer_(consumer) {}
   ~FakeProducerDelegate() override = default;
 
   void Initialize(base::TaskRunner* task_runner) override {
-    producer_.reset(
-        new FakeProducer("android.perfetto.FakeProducer", data_, size_));
+    producer_.reset(new FakeProducer("android.perfetto.FakeProducer", data_,
+                                     size_, consumer_));
     producer_->Connect(PRODUCER_SOCKET, task_runner);
   }
 
@@ -102,6 +108,7 @@ class FakeProducerDelegate : public ThreadDelegate {
   std::unique_ptr<FakeProducer> producer_;
   const uint8_t* data_;
   const size_t size_;
+  FakeConsumer* consumer_;
 };
 
 class ServiceDelegate : public ThreadDelegate {
@@ -127,14 +134,10 @@ int FuzzSharedMemory(const uint8_t* data, size_t size) {
   TaskRunnerThread service_thread;
   service_thread.Start(std::unique_ptr<ServiceDelegate>(new ServiceDelegate()));
 
-  TaskRunnerThread producer_thread;
-  producer_thread.Start(std::unique_ptr<FakeProducerDelegate>(
-      new FakeProducerDelegate(data, size)));
-
   // Setup the TraceConfig for the consumer.
   TraceConfig trace_config;
   trace_config.add_buffers()->set_size_kb(8);
-  trace_config.set_duration_ms(10);
+  trace_config.set_duration_ms(1000);
 
   // Create the buffer for ftrace.
   auto* ds_config = trace_config.add_data_sources()->mutable_config();
@@ -147,12 +150,17 @@ int FuzzSharedMemory(const uint8_t* data, size_t size) {
   auto function = [&finish](std::vector<TracePacket> packets, bool has_more) {
     for (auto& p : packets) {
       p.Decode();
-      if (p->test() == "end")
+      if (p->for_testing().str() == "end")
         finish();
     }
   };
   FakeConsumer consumer(trace_config, std::move(function), &task_runner);
   consumer.Connect(CONSUMER_SOCKET);
+
+  TaskRunnerThread producer_thread;
+  producer_thread.Start(std::unique_ptr<FakeProducerDelegate>(
+      new FakeProducerDelegate(data, size, &consumer)));
+
   task_runner.RunUntilCheckpoint("no.more.packets");
   return 0;
 }
