4110410c37daeb009688f2d8f69f58276ac949ba
Sami Kyostila <skyostil@google.com>
2018-02-27 13:48:38 +0000

    [Patchset 5] service: Implement producer and data source unregistration
    Bug: 73806120
    Change-Id: If3ae078340b4f0d212d6624ca8a20b9665b6de2d

--- src/traced/probes/probes_producer.cc
+++ src/traced/probes/probes_producer.cc
@@ -86,7 +86,6 @@ void ProbesProducer::CreateDataSourceInstance(
     DataSourceInstanceID id,
     const DataSourceConfig& source_config) {
   instances_[id] = source_config.name();
-  // PERFETTO_LOG("DataSourceInstanceID: %llu", id);
   if (source_config.name() == kFtraceSourceName) {
     CreateFtraceDataSourceInstance(id, source_config);
   } else if (source_config.name() == kProcessStatsSourceName) {
@@ -134,6 +133,14 @@ void ProbesProducer::CreateFtraceDataSourceInstance(
   PERFETTO_CHECK(sink);
   delegate->sink(std::move(sink));
   delegates_.emplace(id, std::move(delegate));
+  // Building on Android, watchdogs_.emplace(id, 2* source_config.duration_ms())
+  // does not compile. Presumably, this is due to some detail in its libc++.
+  // Add constant (5 s, semi arbitrarily) to account for very short trace
+  // durations.
+  if (source_config.trace_duration_ms() != 0)
+    watchdogs_.emplace(
+        std::piecewise_construct, std::forward_as_tuple(id),
+        std::forward_as_tuple(5000 + 2 * source_config.trace_duration_ms()));
 }
 
 void ProbesProducer::CreateProcessStatsDataSourceInstance(
@@ -173,6 +180,8 @@ void ProbesProducer::TearDownDataSourceInstance(DataSourceInstanceID id) {
   if (instances_[id] == kFtraceSourceName) {
     size_t removed = delegates_.erase(id);
     PERFETTO_DCHECK(removed == 1);
+    // Might return 0 if trace_duration_ms == 0.
+    watchdogs_.erase(id);
   }
 }
 
