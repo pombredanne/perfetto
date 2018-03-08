0811ed24c7d6d66b90d871bf1c887747ab53f04f
Florian Mayer <fmayer@google.com>
2018-03-08 17:47:23 +0000

    [Patchset 2] Add shell script to run clang-tidy.
    Bug: 74389282
    Change-Id: Idd9fea416440d7b87b3fb61eb752a816bc703b05

---
+++ tools/run-clang-tidy.sh
@@ -0,0 +1,31 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cd "$(dirname "$0")/..";
+affected_files=$(python tools/get-affected-files.py '../../' | grep '.cc$');
+cd "$(dirname "$0")";
+
+if [[ ! -z "${affected_files// }" ]]; then
+  gn gen clang_tidy_build \
+    --args="is_debug=true is_hermetic_clang=false use_custom_libcxx=false";
+  cd clang_tidy_build;
+  cp ../../.clang-tidy .;
+  ninja -t compdb cc cxx obj objcxx > compile_commands.json;
+  # We need to build in order to generate proto headers.
+  ninja;
+  echo $affected_files | xargs -P100 ~/buildclang/build/bin/clang-tidy \
+    '-header-filter=^'"$(realpath ../../)"'/(?!buildtools).*' -p=. -fix;
+else
+  echo "No .cc files affected."
+fi
