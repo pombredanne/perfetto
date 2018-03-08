e78b518755bd802d1ef4be86d5cc4c1bcb23c9a0
Florian Mayer <fmayer@google.com>
2018-03-08 11:47:55 +0000

    [Patchset 13] Add utility function to get block device -> mountpoints.
    Change-Id: I996580d398a1ae7323d1fcbb4a792af53f22c8f3

---
+++ src/base/file_utils.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "perfetto/base/file_utils.h"
+
+#include "perfetto/base/logging.h"
+#include "perfetto/base/scoped_file.h"
+
+namespace perfetto {
+namespace base {
+namespace {
+const size_t kBufSize = 2048;
+}
+
+bool ReadFile(const std::string& path, std::string* out) {
+  base::ScopedFile fd = base::OpenFile(path.c_str(), O_RDONLY);
+  if (!fd)
+    PERFETTO_PLOG("open");
+
+  char buf[kBufSize + 1];
+
+  ssize_t bytes_read;
+  do {
+    bytes_read = PERFETTO_EINTR(read(fd.get(), &buf, kBufSize));
+    if (bytes_read > 0) {
+      buf[bytes_read] = '\0';
+      *out += buf;
+    }
+  } while (bytes_read > 0);
+
+  if (bytes_read == -1) {
+    PERFETTO_PLOG("read");
+    return false;
+  }
+  return true;
+}
+
+}  // namespace base
+}  // namespace perfetto
