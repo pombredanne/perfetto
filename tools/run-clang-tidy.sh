063b770e73c6befe13f76c5ed56c575e78e20b7c
Florian Mayer <fmayer@google.com>
2018-03-08 17:58:37 +0000

    [Patchset 5] Add shell script to run clang-tidy.
    Bug: 74389282
    Change-Id: Idd9fea416440d7b87b3fb61eb752a816bc703b05

--- tools/run-clang-tidy.sh
+++ tools/run-clang-tidy.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Let's not run this against dirty working trees for now.
+git diff-index --quiet HEAD -- ||
+  echo "Untracked changes. Stopping. Please commit your changes"; exit 1;
 
 cd "$(dirname "$0")/..";
 affected_files=$(python tools/get-affected-files.py '../../' | grep '.cc$');
