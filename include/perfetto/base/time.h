19eb840dae4a745a1838682ff1f36f3450c07e18
Primiano Tucci <primiano@google.com>
2018-03-14 13:46:26 +0000

    [Patchset 1] Add base/time.h
    Introduces GetWallTime and GetThreadTime.
    The latter is required for benchmarks.
    
    Bug: 73612642
    Change-Id: I3eea8bcc3b3a98c10c1fad7d99875b5f4fd2ab2f

---
+++ include/perfetto/base/time.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INCLUDE_PERFETTO_BASE_TIME_H_
+#define INCLUDE_PERFETTO_BASE_TIME_H_
+
+#include <stdint.h>
+#include <time.h>
+
+#include "perfetto/base/logging.h"
+
+namespace perfetto {
+namespace base {
+
+using TimeMillis = uint64_t;
+using TimeNanos = uint64_t;
+
+inline TimeNanos GetTimerInternalNs(clockid_t clk_id) {
+  struct timespec ts = {};
+  PERFETTO_CHECK(clock_gettime(clk_id, &ts) == 0);
+  return static_cast<uint64_t>(ts.tv_sec) * 1000000000UL +
+         static_cast<uint64_t>(ts.tv_nsec);
+}
+
+inline TimeMillis GetWallTimeNs() {
+  return GetTimerInternalNs(CLOCK_MONOTONIC_RAW);
+}
+
+inline TimeMillis GetWallTimeMs() {
+  return GetWallTimeNs() / 1000000UL;
+}
+
+inline TimeNanos GetThreadCPUTimeNs() {
+  return GetTimerInternalNs(CLOCK_THREAD_CPUTIME_ID);
+}
+
+}  // namespace base
+}  // namespace perfetto
+
+#endif  // INCLUDE_PERFETTO_BASE_TIME_H_
