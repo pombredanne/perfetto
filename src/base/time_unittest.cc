33f2ab157ab03050281ba71e4055baec37d700f1
Primiano Tucci <primiano@google.com>
2018-03-15 07:21:56 +0000

    [Patchset 19] Add base/time.h
    Introduces GetWallTime() and GetThreadTime().
    The latter is required for benchmarks.
    
    Bug: 73612642
    Test: perfetto_unittests --gtest_filter=TimeTest*
    Change-Id: I3eea8bcc3b3a98c10c1fad7d99875b5f4fd2ab2f

--- src/base/time_unittest.cc
+++ src/base/time_unittest.cc
@@ -43,7 +43,7 @@ TEST(TimeTest, Conversions) {
     EXPECT_EQ(345000000, ts.tv_nsec);
   }
   {
-    struct timespec ts = ToPosixTimespec(TimeMillis(1000000000000001L));
+    struct timespec ts = ToPosixTimespec(TimeMillis(1000000000000001LL));
     EXPECT_EQ(1000000000000L, ts.tv_sec);
     EXPECT_EQ(1000000, ts.tv_nsec);
   }
