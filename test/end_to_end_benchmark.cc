e89c79d10fe07c8de2a0dcda3a37ae002db43cda
Lalit Maganti <lalitm@google.com>
2018-03-29 00:23:00 +0100

    [Patchset 1] DO NOT SUBMIT: perfetto: refactor test code to consolidate pattern
    With several types of testing now present, a clear pattern has been
    established in how test code for end to end tests should be written.
    This pattern involves spinning up service and producer, setting up a
    test-specific trace config and then defining an intricate set of
    checkpoints and RunUntilCheckpoint calls.
    
    All of this code is duplicated and this will only get worse as we add
    more benchmarks and tests to the codebase. Moreover, the
    complex-appearance of the code hides the true simplicity within.
    
    Refactor all the duplicated code into a helper file and call the
    functions in this class from the four other cases where they are used
    (integration test, fuzzer, benchmark and CTS).
    
    TODO: CTS still needs to be updated and tested. Similarily with fuzzer.
    Also fake consumer needs to be deleted.
    
    Bug: 74380167
    Change-Id: I261636b3da15b16f063e6a5dc6c293080b8ff4ac

--- test/end_to_end_benchmark.cc
+++ test/end_to_end_benchmark.cc
@@ -26,21 +26,11 @@
 #include "test/fake_consumer.h"
 #include "test/task_runner_thread.h"
 #include "test/task_runner_thread_delegates.h"
+#include "test/test_helper.h"
 
 namespace perfetto {
 
 namespace {
-
-// If we're building on Android and starting the daemons ourselves,
-// create the sockets in a world-writable location.
-#if PERFETTO_BUILDFLAG(PERFETTO_OS_ANDROID) && \
-    PERFETTO_BUILDFLAG(PERFETTO_START_DAEMONS)
-#define TEST_PRODUCER_SOCK_NAME "/data/local/tmp/traced_producer"
-#define TEST_CONSUMER_SOCK_NAME "/data/local/tmp/traced_consumer"
-#else
-#define TEST_PRODUCER_SOCK_NAME PERFETTO_PRODUCER_SOCK_NAME
-#define TEST_CONSUMER_SOCK_NAME PERFETTO_CONSUMER_SOCK_NAME
-#endif
 
 static bool IsBenchmarkFunctionalOnly() {
   return getenv("BENCHMARK_FUNCTIONAL_TEST_ONLY") != nullptr;
@@ -49,25 +39,11 @@ static bool IsBenchmarkFunctionalOnly() {
 static void BenchmarkCommon(benchmark::State& state) {
   base::TestTaskRunner task_runner;
 
-#if PERFETTO_BUILDFLAG(PERFETTO_START_DAEMONS)
-  TaskRunnerThread service_thread("perfetto.svc");
-  service_thread.Start(std::unique_ptr<ServiceDelegate>(
-      new ServiceDelegate(TEST_PRODUCER_SOCK_NAME, TEST_CONSUMER_SOCK_NAME)));
-#endif
+  TestHelper helper(&task_runner);
+  helper.StartServiceIfRequired();
 
-  TaskRunnerThread producer_thread("perfetto.prd");
-  auto on_producer_enabled = task_runner.CreateCheckpoint("producer.enabled");
-  auto posted_on_producer_enabled = [&task_runner, &on_producer_enabled] {
-    task_runner.PostTask(on_producer_enabled);
-  };
-  std::unique_ptr<FakeProducerDelegate> producer_delegate(
-      new FakeProducerDelegate(TEST_PRODUCER_SOCK_NAME,
-                               std::move(posted_on_producer_enabled)));
-  FakeProducerDelegate* producer_delegate_cached = producer_delegate.get();
-  producer_thread.Start(std::move(producer_delegate));
-
-  // Once conneced, we can retrieve the inner producer.
-  FakeProducer* producer = producer_delegate_cached->producer();
+  FakeProducer* producer = helper.ConnectFakeProducer();
+  helper.ConnectConsumer();
 
   // Setup the TraceConfig for the consumer.
   TraceConfig trace_config;
@@ -94,61 +70,35 @@ static void BenchmarkCommon(benchmark::State& state) {
   ds_config->mutable_for_testing()->set_message_size(message_bytes);
   ds_config->mutable_for_testing()->set_max_messages_per_second(messages_per_s);
 
-  bool is_first_packet = true;
-  auto on_readback_complete = task_runner.CreateCheckpoint("readback.complete");
-  std::minstd_rand0 rnd_engine(kRandomSeed);
-  auto on_consumer_data = [&is_first_packet, &on_readback_complete,
-                           &rnd_engine](std::vector<TracePacket> packets,
-                                        bool has_more) {
-    for (auto& packet : packets) {
-      ASSERT_TRUE(packet.Decode());
-      ASSERT_TRUE(packet->has_for_testing() || packet->has_clock_snapshot() ||
-                  packet->has_trace_config());
-      if (packet->has_clock_snapshot() || packet->has_trace_config())
-        continue;
-      ASSERT_EQ(protos::TracePacket::kTrustedUid,
-                packet->optional_trusted_uid_case());
-      if (is_first_packet) {
-        rnd_engine = std::minstd_rand0(packet->for_testing().seq_value());
-        is_first_packet = false;
-      } else {
-        ASSERT_EQ(packet->for_testing().seq_value(), rnd_engine());
-      }
-    }
+  helper.StartTracing(trace_config);
 
-    if (!has_more) {
-      is_first_packet = true;
-      on_readback_complete();
+  bool is_first_packet = true;
+  std::minstd_rand0 rnd_engine(kRandomSeed);
+  auto on_consumer_data = [&is_first_packet, &rnd_engine](
+                              const TracePacket::DecodedTracePacket& packet) {
+    ASSERT_TRUE(packet.has_for_testing());
+    if (is_first_packet) {
+      rnd_engine = std::minstd_rand0(packet.for_testing().seq_value());
+      is_first_packet = false;
+    } else {
+      ASSERT_EQ(packet.for_testing().seq_value(), rnd_engine());
     }
   };
 
-  // Finally, make the consumer connect to the service.
-  auto on_connect = task_runner.CreateCheckpoint("consumer.connected");
-  FakeConsumer consumer(trace_config, std::move(on_connect),
-                        std::move(on_consumer_data), &task_runner);
-  consumer.Connect(TEST_CONSUMER_SOCK_NAME);
-  task_runner.RunUntilCheckpoint("consumer.connected");
-
-  consumer.EnableTracing();
-  task_runner.RunUntilCheckpoint("producer.enabled");
-
   uint64_t wall_start_ns = base::GetWallTimeNs().count();
-  uint64_t service_start_ns = service_thread.GetThreadCPUTimeNs();
-  uint64_t producer_start_ns = producer_thread.GetThreadCPUTimeNs();
+  uint64_t service_start_ns = helper.service_thread()->GetThreadCPUTimeNs();
+  uint64_t producer_start_ns = helper.producer_thread()->GetThreadCPUTimeNs();
   uint64_t iterations = 0;
   for (auto _ : state) {
     auto cname = "produced.and.committed." + std::to_string(iterations++);
     auto on_produced_and_committed = task_runner.CreateCheckpoint(cname);
-    auto posted_on_produced_and_committed = [&task_runner,
-                                             &on_produced_and_committed] {
-      task_runner.PostTask(on_produced_and_committed);
-    };
-    producer->ProduceEventBatch(posted_on_produced_and_committed);
+    producer->ProduceEventBatch(helper.WrapTask(on_produced_and_committed));
     task_runner.RunUntilCheckpoint(cname, time_for_messages_ms);
   }
-  uint64_t service_ns = service_thread.GetThreadCPUTimeNs() - service_start_ns;
+  uint64_t service_ns =
+      helper.service_thread()->GetThreadCPUTimeNs() - service_start_ns;
   uint64_t producer_ns =
-      producer_thread.GetThreadCPUTimeNs() - producer_start_ns;
+      helper.producer_thread()->GetThreadCPUTimeNs() - producer_start_ns;
   uint64_t wall_ns = base::GetWallTimeNs().count() - wall_start_ns;
 
   state.counters["Pro CPU"] = benchmark::Counter(100.0 * producer_ns / wall_ns);
@@ -157,13 +107,12 @@ static void BenchmarkCommon(benchmark::State& state) {
       benchmark::Counter(1.0 * service_ns / message_count);
 
   // Read back the buffer just to check correctness.
-  consumer.ReadTraceData();
+  auto on_readback_complete = task_runner.CreateCheckpoint("readback.complete");
+  helper.ReadData(on_consumer_data, on_readback_complete);
   task_runner.RunUntilCheckpoint("readback.complete");
   state.SetBytesProcessed(iterations * message_bytes * message_count);
-
-  consumer.Disconnect();
 }
-}
+}  // namespace
 
 static void BM_EndToEnd_SaturateCpu(benchmark::State& state) {
   BenchmarkCommon(state);
---
+++ test/test_helper.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TEST_TEST_HELPER_H_
+#define TEST_TEST_HELPER_H_
+
+#include "perfetto/tracing/core/trace_config.h"
+#include "src/base/test/test_task_runner.h"
+#include "test/fake_consumer.h"
+#include "test/fake_producer.h"
+#include "test/task_runner_thread.h"
+
+namespace perfetto {
+
+class TestHelper : public Consumer {
+ public:
+  explicit TestHelper(base::TestTaskRunner* task_runner);
+
+  // Consumer implementation.
+  void OnConnect() override;
+  void OnDisconnect() override;
+  void OnTracingStop() override;
+  void OnTraceData(std::vector<TracePacket> packets, bool has_more) override;
+
+  void StartServiceIfRequired();
+  FakeProducer* ConnectFakeProducer();
+  void ConnectConsumer();
+  void StartTracing(const TraceConfig& config);
+  void ReadData(std::function<void(const TracePacket::DecodedTracePacket&)>
+                    packet_callback,
+                std::function<void()> on_finish_callback);
+
+  std::function<void()> WrapTask(const std::function<void()>& function);
+
+  TaskRunnerThread* service_thread() { return &service_thread_; }
+  TaskRunnerThread* producer_thread() { return &producer_thread_; }
+
+ private:
+  base::TestTaskRunner* task_runner_ = nullptr;
+
+  std::function<void(const TracePacket::DecodedTracePacket&)> packet_callback_;
+  std::function<void()> continuation_callack_;
+
+  TaskRunnerThread service_thread_;
+  TaskRunnerThread producer_thread_;
+  std::unique_ptr<Service::ConsumerEndpoint> endpoint_;  // Keep last.
+};
+
+}  // namespace perfetto
+
+#endif  // TEST_TEST_HELPER_H_
