a6336cf73e0318a1dde66ebecf51eeb1f4100588
Isabelle Taylor <taylori@google.com>
2018-03-28 15:45:14 +0100

    [Patchset 4] Filter producers for each data source.
    Data sources specify in the producer_name_filter which producers they care about.
    
    Bug:73625220
    Change-Id: Ibb190d3fbc843c3d70e18534ac13a7dd386a1bd7

--- src/tracing/core/service_impl.cc
+++ src/tracing/core/service_impl.cc
@@ -699,14 +699,16 @@ void ServiceImpl::CreateDataSourceInstance(
   }
   // TODO(primiano): Add tests for registration ordering
   // (data sources vs consumers).
-  if (std::find(cfg_data_source.producer_name_filter().begin(),
-                cfg_data_source.producer_name_filter().end(),
-                producer->name_) ==
-      cfg_data_source.producer_name_filter().end()) {
-    PERFETTO_DLOG("Data source: %s is not enabled for producer: %s",
-                  cfg_data_source.config().name().c_str(),
-                  producer->name_.c_str());
-    return;
+  if (!cfg_data_source.producer_name_filter().empty()) {
+    if (std::find(cfg_data_source.producer_name_filter().begin(),
+                  cfg_data_source.producer_name_filter().end(),
+                  producer->name_) ==
+        cfg_data_source.producer_name_filter().end()) {
+      PERFETTO_DLOG("Data source: %s is not enabled for producer: %s",
+                    cfg_data_source.config().name().c_str(),
+                    producer->name_.c_str());
+      return;
+    }
   }
 
   // Create a copy of the DataSourceConfig specified in the trace config. This
