45996ebfbb610738e4f3ad4bfbd4626364dd590e
Primiano Tucci <primiano@google.com>
2018-03-29 18:32:02 +0100

    [Patchset 9] Allow to configure InodeFileDataSource.
    Bug: 74584014
    Change-Id: I4f3afd96e7e017e2ca2a10aac661460a97d4f90c

--- include/perfetto/tracing/core/data_source_config.h
+++ include/perfetto/tracing/core/data_source_config.h
@@ -86,6 +86,9 @@ class PERFETTO_EXPORT DataSourceConfig {
   }
   InodeFileConfig* mutable_inode_file_config() { return &inode_file_config_; }
 
+  const std::string& legacy_config() const { return legacy_config_; }
+  void set_legacy_config(const std::string& value) { legacy_config_ = value; }
+
   const TestConfig& for_testing() const { return for_testing_; }
   TestConfig* mutable_for_testing() { return &for_testing_; }
 
@@ -96,6 +99,7 @@ class PERFETTO_EXPORT DataSourceConfig {
   FtraceConfig ftrace_config_ = {};
   ChromeConfig chrome_config_ = {};
   InodeFileConfig inode_file_config_ = {};
+  std::string legacy_config_ = {};
   TestConfig for_testing_ = {};
 
   // Allows to preserve unknown protobuf fields for compatibility
