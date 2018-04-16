bfc84cd47885e5d38475a0ca7e82bb9bf11a55a4
Primiano Tucci <primiano@google.com>
2018-04-15 20:08:28 -0700

    [Patchset 2] WASM experiments: Run c++ protobuf code in the browser
    WIP, but works-ish, 16 MB/s in debug mode. Crashes in release mode.
    
    Change-Id: I1f950dd256da07e18aa8868c40d4dd8f46e2049d

--- src/analysis/test.cc
+++ src/analysis/test.cc
@@ -11,11 +11,23 @@
 
 using namespace perfetto;
 
-extern "C" uint32_t FetchTrace(uint32_t offset, uint32_t length);
+namespace {
+constexpr uint32_t kIOBufSize = 1 * 1024 * 1024;
+}  // namespace.
 
+// Imports (functions defined in JS)
+extern "C" uint32_t FetchTrace(uint32_t offset, uint32_t length);
+extern "C" void TraceStatusUpdate(uint32_t bytes_loaded,
+                                  bool complete,
+                                  float duration_ms);
+
+// Exports (functions exported to JS)
 extern "C" void EMSCRIPTEN_KEEPALIVE PerfettoLoadTrace();
-extern "C" void EMSCRIPTEN_KEEPALIVE PerfettoOnTraceFetched(const char* data,
-                                                            uint32_t size);
+extern "C" void EMSCRIPTEN_KEEPALIVE PerfettoOnTraceFetched(uint32_t size);
+extern "C" uint32_t EMSCRIPTEN_KEEPALIVE LookupFtracePacket(float timestamp_ms);
+extern "C" void EMSCRIPTEN_KEEPALIVE PrintPacketsAt(float timestamp_ms);
+extern "C" char* EMSCRIPTEN_KEEPALIVE GetIOBuf();
+extern "C" uint32_t EMSCRIPTEN_KEEPALIVE GetIOBufSize();
 
 // -----------------------------------------------------------------------------
 // TraceStorage
@@ -76,7 +88,7 @@ void TraceStorage::OnChunkFetched(const char* data, uint32_t size) {
 }
 
 // -----------------------------------------------------------------------------
-// TraceIndexer
+// FtraceIndex
 // -----------------------------------------------------------------------------
 
 class FtraceIndex {
@@ -91,6 +103,16 @@ class FtraceIndex {
   // Returns the offset of the a packet that starts <= TimeNs and that is
   // reasonably close to it.
   OffsetBytes Lookup(TimeNs);
+
+  TimeNs front() const { return index_.empty() ? 0 : index_.begin()->first; }
+  TimeNs back() const { return index_.empty() ? 0 : index_.rbegin()->first; }
+
+  float duration() const {
+    if (index_.empty())
+      return 0;
+    TimeNs duration = index_.rbegin()->first - index_.begin()->first;
+    return duration / 1000000000.0f;
+  }
 
  private:
   std::map<TimeNs, OffsetBytes> index_;
@@ -122,23 +144,34 @@ FtraceIndex::OffsetBytes FtraceIndex::Lookup(TimeNs ns) {
 
 class TraceLoader {
  public:
+  static TraceLoader* GetInstance();
+  static uint32_t DecodePacket(const char*, size_t, protos::TracePacket*);
+
   void IndexFullTrace();
   void IndexPacket(const protos::TracePacket&, uint32_t offset);
   void FetchNextChunk();
   void OnChunkFetched(const char* data, uint32_t size);
 
+  FtraceIndex* ftrace_index() { return &ftrace_index_; }
+  uint32_t bytes_processed() const { return cur_offset_; }
+
  private:
-  static constexpr uint32_t kChunkSize = 1024 * 1024;
   uint32_t cur_offset_ = 0;
   FtraceIndex ftrace_index_;  // TODO: should be per CPU.
 };
+
+TraceLoader* TraceLoader::GetInstance() {
+  static TraceLoader* instance = new TraceLoader();
+  return instance;
+}
 
 void TraceLoader::IndexFullTrace() {
   cur_offset_ = 0;
   FetchNextChunk();
 }
 
-void TraceLoader::IndexPacket(const protos::TracePacket& packet, uint32_t offset) {
+void TraceLoader::IndexPacket(const protos::TracePacket& packet,
+                              uint32_t offset) {
   switch (packet.data_case()) {
     case protos::TracePacket::kFtraceEvents:
       ftrace_index_.AddPacket(packet.ftrace_events(), offset);
@@ -157,55 +190,64 @@ void TraceLoader::IndexPacket(const protos::TracePacket& packet, uint32_t offset
 
 void TraceLoader::FetchNextChunk() {
   TraceStorage::GetInstance()->FetchChunk(
-      cur_offset_, kChunkSize,
+      cur_offset_, kIOBufSize,
       [this](const char* data, uint32_t size) { OnChunkFetched(data, size); });
+}
+
+uint32_t TraceLoader::DecodePacket(const char* data,
+                                   size_t len,
+                                   protos::TracePacket* packet) {
+  if (len < 5)
+    return 0;
+
+  const char* p = data;
+  if (*(p++) != 0x0a)  // Field ID:1, type:length delimited.
+    abort();
+
+  // Read the varint size.
+  uint32_t packet_size = 0;
+  uint32_t shift = 0;
+  for (;;) {
+    char c = *(p++);
+    packet_size |= static_cast<uint32_t>(c & 0x7f) << shift;
+    shift += 7;
+    if (!(c & 0x80))
+      break;
+  }
+
+  if (len < packet_size)
+    return 0;
+
+  // printf("Decoding packet %p - %u\n", reinterpret_cast<const void*>(p),
+  //        packet_size);
+  // for (uint32_t i = 0; i < packet_size; i++)
+  //   printf("%02x ", p[i] & 0xff);
+  // printf("\n");
+  if (!packet->ParseFromArray(p, static_cast<int>(packet_size))) {
+    printf("Failed to read packet %p - %u\n", reinterpret_cast<const void*>(p),
+           packet_size);
+    abort();
+  }
+  return static_cast<uint32_t>(p - data) + packet_size;
 }
 
 void TraceLoader::OnChunkFetched(const char* data, uint32_t size) {
   // printf("Fetched %u @ %u\n", size, cur_offset_);
 
-  if (size == 0)
+  bool complete = size == 0;
+  TraceStatusUpdate(cur_offset_, complete, ftrace_index_.duration());
+  if (complete)
     return;
 
   const char* p = data;
 
-  auto bytes_left = [data, size, &p]() -> uint32_t {
-    return size - static_cast<uint32_t>(p - data);
-  };
-
   for (;;) {
-    if (bytes_left() < 5)
-      break;
-
-    const char* last_packet_start = p;
-
-    // TOOD check bytes left > 5.
-    assert(*(p++) == 0x0a);  // Field ID:1, type:length delimited.
-
-    // Read the varint size.
-    uint32_t packet_size = 0;
-    uint32_t shift = 0;
-    for (;;) {
-      char c = *(p++);
-      packet_size |= static_cast<uint32_t>(c & 0x7f) << shift;
-      shift += 7;
-      if (!(c & 0x80))
-        break;
-    }
-
-    if (bytes_left() < packet_size) {
-      p = last_packet_start;
-      break;
-    }
-
+    uint32_t bytes_left = size - static_cast<uint32_t>(p - data);
     protos::TracePacket packet;
-    if (!packet.ParseFromArray(p, static_cast<int>(packet_size))) {
-      printf("Failed to read packet, left: %u\n", bytes_left());
-      abort();
-    }
+    uint32_t packet_size = TraceLoader::DecodePacket(p, bytes_left, &packet);
+    if (packet_size == 0)
+      break;
     IndexPacket(packet, cur_offset_ + static_cast<uint32_t>(p - data));
-    // printf("Read packet @ %u, len: %u\n",
-           // cur_offset_ + static_cast<uint32_t>(p - data), packet_size);
     p += packet_size;
   }
 
@@ -218,18 +260,61 @@ void TraceLoader::OnChunkFetched(const char* data, uint32_t size) {
 // -----------------------------------------------------------------------------
 
 extern "C" void PerfettoLoadTrace() {
-  static TraceLoader* trace_loader = new TraceLoader();
-  trace_loader->IndexFullTrace();
+  TraceLoader::GetInstance()->IndexFullTrace();
 }
 
-extern "C" void PerfettoOnTraceFetched(const char* data, uint32_t size) {
-  TraceStorage::GetInstance()->OnChunkFetched(data, size);
+extern "C" void PerfettoOnTraceFetched(uint32_t size) {
+  TraceStorage::GetInstance()->OnChunkFetched(GetIOBuf(), size);
+}
+
+extern "C" uint32_t LookupFtracePacket(float timestamp_ms) {
+  FtraceIndex* fi = TraceLoader::GetInstance()->ftrace_index();
+  uint64_t ns = fi->front() + static_cast<uint64_t>(timestamp_ms * 1000000);
+  return fi->Lookup(ns);
+}
+
+extern "C" void PrintPacketsAt(float timestamp_ms) {
+  FtraceIndex* fi = TraceLoader::GetInstance()->ftrace_index();
+  uint64_t ns = fi->front() + static_cast<uint64_t>(timestamp_ms * 1000000);
+  uint32_t off = fi->Lookup(ns);
+  printf("Printing packets @ T = %u (file offset: %u)\n\n",
+         static_cast<uint32_t>(timestamp_ms), off);
+  TraceStorage::GetInstance()->FetchChunk(
+      off, 1024 * 64, [](const char* data, uint32_t size) {
+        protos::TracePacket packet;
+        uint32_t res = TraceLoader::DecodePacket(data, size, &packet);
+        if (!res) {
+          printf("Failed to parse packet\n");
+          return;
+        }
+        uint64_t trace_start =
+            TraceLoader::GetInstance()->ftrace_index()->front();
+        const protos::FtraceEventBundle& bundle = packet.ftrace_events();
+        for (const protos::FtraceEvent& event : bundle.event()) {
+          if (!event.has_sched_switch())
+            continue;
+          const auto& ss = event.sched_switch();
+          printf("%-9.2lf ms: %s -> %s\n",
+                 (event.timestamp() - trace_start) / 1000000.0,
+                 ss.prev_comm().c_str(), ss.next_comm().c_str());
+        }
+      });
+}
+
+extern "C" char* GetIOBuf() {
+  static char* iobuf = static_cast<char*>(malloc(kIOBufSize));
+  return iobuf;
+}
+
+extern "C" uint32_t GetIOBufSize() {
+  return kIOBufSize;
 }
 
 int main() {
   printf("WASM runtime ready\n");
   return 0;
 }
+
 // extern "C" void PerfettoProcessTrace(const char* mem, int size) {
 //   printf("Parsing trace in C++ (mem: %p, size: %d)\n",
 //          reinterpret_cast<const void*>(mem), size);
--- ui/lib.js
+++ ui/lib.js
@@ -1,5 +1,8 @@
 mergeInto(LibraryManager.library, {
   FetchTrace: function(offset, length) {
     FetchTrace(offset, length);
-  }
+  },
+  TraceStatusUpdate: function(bytes_loaded, complete, duration) {
+    TraceStatusUpdate(bytes_loaded, complete, duration);
+  },
 });
