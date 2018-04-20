f15f2428220365358f91ce140f53d0a799b8d303
Florian Mayer <fmayer@google.com>
2018-04-20 14:51:17 +0100

    [Patchset 1] Turn off clang-format for autogen files.
    Turn off LongLine check as well.
    
    Bug: 77844626
    Change-Id: I5464cb3d47f62c496e923226b5aab17173b4dad2

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -21,7 +21,9 @@ def CheckChange(input, output):
     # There apparently is no way to wrap strings in blueprints, so ignore long
     # lines in them.
     long_line_sources = lambda x: input.FilterSourceFile(
-            x, white_list=".*", black_list=['Android[.]bp'])
+            x, white_list=".*", black_list=
+            ['Android[.]bp', 'protos/perfetto/trace/ftrace/ftrace_event.proto',
+             'protos/perfetto/trace/ftrace/.*[.]proto'])
     results = []
     results += input.canned_checks.CheckDoNotSubmit(input, output)
     results += input.canned_checks.CheckChangeHasNoTabs(input, output)
