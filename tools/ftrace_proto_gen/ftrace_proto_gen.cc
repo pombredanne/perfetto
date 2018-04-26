67c32e31c73ff02dcb95dc4aec4a53cdc8b30578
Florian Mayer <fmayer@google.com>
2018-04-26 11:45:08 +0100

    [Patchset 1] Comment.

--- tools/ftrace_proto_gen/ftrace_proto_gen.cc
+++ tools/ftrace_proto_gen/ftrace_proto_gen.cc
@@ -111,8 +111,8 @@ ProtoType ProtoType::Numeric(uint16_t size, bool is_signed) {
 }
 
 ProtoType GetCommon(ProtoType one, ProtoType other) {
-  // If RHS is STRING but LHS isn't, we need to be it as a NUMERIC because
-  // we can't safely convert a NUMERIC into a string.
+  // Always need to prefer the LHS as it is the one already present
+  // in the proto.
   if (one.type == ProtoType::STRING)
     return ProtoType::String();
 
