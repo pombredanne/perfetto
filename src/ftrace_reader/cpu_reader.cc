3bfc8a2a08b08be2476fbc001d3ba437a610b56d
Florian Mayer <fmayer@google.com>
2018-04-27 11:19:16 +0100

    [Patchset 2] Add handling to parse __data_loc.
    Bug: 78222143
    Change-Id: I048482bd2a24203fc043db2643dc6750843c28f6

--- src/ftrace_reader/cpu_reader.cc
+++ src/ftrace_reader/cpu_reader.cc
@@ -464,6 +464,7 @@ bool CpuReader::ParseField(const Field& field,
       // TODO(hjd): Kernel-dive to check this how size:0 char fields work.
       return ReadIntoString(field_start, end, field.proto_field_id, message);
     case kStringPtrToString:
+      // TODO(hjd): Figure out how to read these.
       return true;
     case kDataLocToString: {
       if (field.ftrace_size != 4) {
