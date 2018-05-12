501a5050ea92f631c5e35593a3ed26eab2c315e0
Primiano Tucci <primiano@google.com>
2018-05-12 15:49:07 +0100

    [Patchset 3] WIP Add JS<>WASM TraceProcessor
    Still work in progress.
    
    Change-Id: I6aeeb89ca8e750d3665147fb39314e2592052ac6

---
+++ ui/main.ts
@@ -0,0 +1,8 @@
+import * as X from "./lib/query"
+
+// import * as S from "./lib/sched";
+
+// declare const protobuf: Protobuf.Root;
+// declare const protos: S.perfetto.protos;
+// new S.perfetto.protos.Query()
+new X.perfetto.protos.Query()
--- ui/trace_processor.js
+++ ui/trace_processor.js
@@ -2,35 +2,44 @@
 
 // TODO: this should be written in typescript.
 
-// This will become the JS version of
-// /protos/perfetto/processed_trace/query.proto.
-var Query = undefined;
-protobuf.load('/protos/perfetto/processed_trace/query.proto').then((root) => {
-  Query = root.lookupType('perfetto.protos.Query');
-  console.log('Protobuf types loaded');
-});
-
-// This class (or at least, its methods) will be auto-generated.
+// Forwards calls to the worker.
 class TraceProcessor {
   // |file| is a File object returned by HTML5 File API.
   constructor(file) {
+    this.protobuf = new protobuf.Root();
+    this.protobuf.resolvePath = (_, includePath) => '/protos/' + includePath;
+    // this.pbRoot.load(['perfetto/processed_trace/sched.proto']);
+
     this.worker = new Worker('worker.js');
     this.worker.addEventListener('message', this.onWorkerMessage.bind(this));
     this.pendingRequests = {};
     this.lastReqId = 0;
   }
 
-  getSlices(query) {
-    // (Aaaand this is why I hate JS so much).
-    console.assert(query.__proto__ == Query.create().__proto__);
-
-    var protobuf_encoded_query = Query.encode(query).finish();
-    this.workerAsyncCall('GetSlices', protobuf_encoded_query)
-        .then((x) => { console.log('getSlices reply: ', x); });
+  // This function returns a JS object that matches perfectly the RPC service
+  // described in //protos/perfetto/processed_trace/|name|.proto.
+  // Example:
+  // getInterface('sched').then(schedSvc => { schedSvc.GetSchedEvents(...); });
+  getInterface(name) {
+    return new Promise((resolve, reject) => {
+      const path = 'perfetto/processed_trace/' + name + '.proto';
+      this.protobuf.load(path).then(root => {
+        try {
+          const svc = root.lookupService('perfetto.protos.' + name);
+          resolve([svc.create(this.rpcImpl.bind(this)), svc.parent]);
+        } catch(err) {
+          reject(err);
+        }
+      });
+    });
   }
 
-// private:
-  workerAsyncCall(funcName, protoInput) {
+  rpcImpl(method, requestData, callback) {
+    console.log(requestData);
+  };
+
+  // Sends a RPC call to the worker. Also handles bookkeeping for the reply.
+  asyncCall(funcName, protoInput) {
     return new Promise((resolve, reject) => {
       const reqId = ++this.lastReqId;
       this.pendingRequests[reqId] = {resolve: resolve,
@@ -59,4 +68,4 @@ class TraceProcessor {
 
 }
 
-var p = new TraceProcessor('trace_name.protobuf');
+var tp = new TraceProcessor('trace_name.protobuf');
