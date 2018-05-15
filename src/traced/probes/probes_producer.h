516ff062856c62173fda60c41260ab2ab89343b7
Primiano Tucci <primiano@google.com>
2018-05-15 08:35:44 +0100

    [Patchset 3] Roll GN and libc++ to match chromium revisions
    Updates the GN binary and the libc++, libc++abi and libunwind
    to match the most recent versions rolled into chrome.
    Has no effect on the Android tree builds.
    
    Change-Id: I0930b957099da78c8d5ebfebfd8dd387638496fd

--- src/traced/probes/probes_producer.h
+++ src/traced/probes/probes_producer.h
@@ -25,6 +25,7 @@
 #include "perfetto/base/watchdog.h"
 #include "perfetto/ftrace_reader/ftrace_controller.h"
 #include "perfetto/tracing/core/producer.h"
+#include "perfetto/tracing/core/service.h"
 #include "perfetto/tracing/core/trace_writer.h"
 #include "src/traced/probes/filesystem/inode_file_data_source.h"
 #include "src/traced/probes/process_stats_data_source.h"
