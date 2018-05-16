a31729c652c478298e4baecb63e95804cb58ed1b
Deepanjan Roy <dproy@google.com>
2018-05-16 17:15:34 -0400

    [Patchset 2] WIP
    Change-Id: I98c193ae196ed1d487ccbb6316bd1d50c5666559

--- ui/bs-config.js
+++ ui/bs-config.js
@@ -5,33 +5,44 @@ const print = data => process.stdout.write(data);
 const printErr = data => process.stderr.write(data);
 
 const ninjaOutDir = process.env.NINJA_OUT_DIR;
+let ninjaRunning = false;
 
 module.exports = function(bs) {
   return {
     files: [
       {
         // TODO: Update this file list. This is only an example.
-        match: ["ui/*", "src/trace_processor/*"],
+        match: ["ui/**", "src/trace_processor/**", "protos/**"],
         fn: function(event, file) {
           console.log(`Change detected on ${file}`);
-          console.log(`Executing: ninja -C ${ninjaOutDir} ui`);
+          if (ninjaRunning) {
+            console.log("Already have a ninja build running. Doing nothing.");
+            return;
+          }
 
+          ninjaRunning = true;
+
+          console.log(`Executing: ninja -C ${ninjaOutDir} ui`);
           const ninja = spawn('ninja', ['-C', ninjaOutDir, 'ui']);
           ninja.stdout.on('data', data => print(data.toString()));
           ninja.stderr.on('data', data => printErr(data.toString()));
 
-          // TODO: We can be smarter and load just the file we need. Need to
+          // We can be smarter and load just the file we need. Need to
           // resolve to the dist/location of the file in that case.
-          ninja.on('exit', () => bs.reload());
+          // For now, we're reloading the whole page.
+          ninja.on('exit', () => {
+            ninjaRunning = false;
+            bs.reload();
+          });
         },
         options: {
-          ignored: "dist/",
+          ignored: ["ui/dist/", "ui/.git/", "ui/node_modules/"],
           ignoreInitial: true
         }
       }
     ],
     server: {
       baseDir: "ui/dist"
-    }
+    },
   };
 };
