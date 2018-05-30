f2f2af30c2af51a1dddc613aa7a4f1cea46833ec
Lalit Maganti <lalitm@google.com>
2018-05-30 17:27:22 +0100

    trace_processor: add base trace loading code
    This CL sets out the foundation for the trace loading code.
    Specifically, it introduces BlobReader (an abstraction for reading data
    from an arbitary data source into a buffer) and TraceLoader.
    
    TraceLoader will use BlobReader and a class known as ProtoParser (to be
    introduced in a follow-up CL) to read and parse the trace packets and
    place them in the ColumnarTrace introduced in the previous CL
    
    Bug: 80416541
    Change-Id: I5eb725ff6676b3ecb12daba74e8f0dc766eff11e

---
+++ src/trace_processor/trace_loader.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_TRACE_PROCESSOR_TRACE_LOADER_H_
+#define SRC_TRACE_PROCESSOR_TRACE_LOADER_H_
+
+#include <cstdint>
+#include <memory>
+
+#include "src/trace_processor/blob_reader.h"
+#include "src/trace_processor/columnar_trace.h"
+
+namespace perfetto {
+namespace trace_processor {
+
+class TraceLoader {
+ public:
+  TraceLoader(BlobReader* reader, ColumnarTrace* trace, uint32_t chunk_size_b);
+
+  void LoadNextChunk();
+
+ private:
+  BlobReader* const reader_;
+  ColumnarTrace* const trace_;
+  const uint32_t chunk_size_b_;
+
+  uint32_t offset_ = 0;
+  std::unique_ptr<uint8_t[]> buffer_;
+};
+
+}  // namespace trace_processor
+}  // namespace perfetto
+
+#endif  // SRC_TRACE_PROCESSOR_TRACE_LOADER_H_
