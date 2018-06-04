55472746ad6bc842389c83918714d16700a7fb5b
Lalit Maganti <lalitm@google.com>
2018-06-04 18:31:20 +0100

    [Patchset 2] trace_processor: initial sched slice table
    Bug: 80416541
    Change-Id: I563dcbbb8a3bf81eec2bc0afbd962cbcc12fe4e5

--- src/protozero/proto_decoder.cc
+++ src/protozero/proto_decoder.cc
@@ -50,7 +50,7 @@ ProtoDecoder::Field ProtoDecoder::ReadField() {
   PERFETTO_DCHECK(pos <= end);
 
   // If we've already hit the end, just return an invalid field.
-  if (pos == end) {
+  if (pos >= end) {
     return field;
   }
 
@@ -107,8 +107,8 @@ ProtoDecoder::Field ProtoDecoder::ReadField() {
       // We need to explicity check for zero to ensure that ParseVarInt doesn't
       // return zero because of running out of space in the buffer.
       if (*pos == 0) {
-        field.length_value.data = ++pos;
-        field.length_value.length = 0;
+        field.length_limited.data = ++pos;
+        field.length_limited.length = 0;
       } else {
         pos = ParseVarInt(pos, end, &field_intvalue);
 
@@ -120,8 +120,8 @@ ProtoDecoder::Field ProtoDecoder::ReadField() {
         if (field_intvalue == 0 || pos + field_intvalue > end) {
           return field;
         }
-        field.length_value.data = pos;
-        field.length_value.length = field_intvalue;
+        field.length_limited.data = pos;
+        field.length_limited.length = field_intvalue;
         pos += field_intvalue;
       }
       break;
