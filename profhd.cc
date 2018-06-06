252410d52675d782ea9b2ed21c92c7c3a0421200
Florian Mayer <fmayer@google.com>
2018-06-06 14:08:06 +0100

    [Patchset 10] Prototype for out-of-process asyc unwinding.
    Change-Id: Ib1b379fab9bd73f706ecccec2552aac74e4399eb

--- profhd.cc
+++ profhd.cc
@@ -27,13 +27,52 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <algorithm>
 
 #include "perfetto/base/logging.h"
 #include "perfetto/base/unix_task_runner.h"
 #include "perfetto/base/weak_ptr.h"
+#include "perfetto/base/scoped_file.h"
 #include "src/ipc/unix_socket.h"
 
+#include <unwindstack/Regs.h>
+#include <unwindstack/RegsArm.h>
+#include <unwindstack/RegsArm64.h>
+#include <unwindstack/RegsMips.h>
+#include <unwindstack/RegsMips64.h>
+#include <unwindstack/RegsX86.h>
+#include <unwindstack/RegsX86_64.h>
+#include <unwindstack/UserArm.h>
+#include <unwindstack/UserArm64.h>
+#include <unwindstack/UserMips.h>
+#include <unwindstack/UserMips64.h>
+#include <unwindstack/UserX86.h>
+#include <unwindstack/UserX86_64.h>
+#include <unwindstack/Elf.h>
+#include <unwindstack/Memory.h>
+#include <unwindstack/Unwinder.h>
+
+
 namespace perfetto {
+
+class StackMemory : public unwindstack::MemoryRemote {
+	public:
+  StackMemory(pid_t pid, uint64_t sp, uint8_t* stack, size_t size) : MemoryRemote(pid), sp_(sp), size_(size) {}
+
+	size_t Read(uint64_t addr, void* dst, size_t size) override {
+		int64_t offset = addr - sp_;
+		if (offset >= 0 && offset < size_) {
+			PERFETTO_CHECK(size < size_ - offset);
+			memcpy(dst, stack_ + offset, size);
+			return size;
+		}
+		return unwindstack::MemoryRemote::Read(addr, dst, size);
+	}
+	private:
+	uint64_t sp_;
+	uint8_t* stack_;
+	size_t size_;
+};
 
 static long total_read = 0;
 
@@ -42,17 +81,105 @@ struct Metadata {
   uint8_t regs[66];
   int64_t pid;
   uint64_t size;
+	void* sp;
 };
+
+unwindstack::Regs* CreateFromRawData(unwindstack::ArchEnum arch, void* raw_data) {
+  switch (arch) {
+  case unwindstack::ARCH_X86:
+      return unwindstack::RegsX86::Read(raw_data);
+  case unwindstack::ARCH_X86_64:
+      return unwindstack::RegsX86_64::Read(raw_data);
+  case unwindstack::ARCH_ARM:
+      return unwindstack::RegsArm::Read(raw_data);
+  case unwindstack::ARCH_ARM64:
+      return unwindstack::RegsArm64::Read(raw_data);
+  case unwindstack::ARCH_MIPS:
+      return unwindstack::RegsMips::Read(raw_data);
+  case unwindstack::ARCH_MIPS64:
+      return unwindstack::RegsMips64::Read(raw_data);
+  case unwindstack::ARCH_UNKNOWN:
+    default:
+      return nullptr;
+  }
+}
+
+static int total_frames = 0;
+static unsigned long total_records = 0;
 
 void Done(int fd, size_t sz);
 void Done(int fd, size_t sz) {
+	PERFETTO_LOG("perfhd: records: %lu\n", total_records++);
+	return;
   void* mem = mmap(nullptr, sz, PROT_READ, 0, fd, 0);
   Metadata* metadata = reinterpret_cast<Metadata*>(mem);
   uint8_t* stack = reinterpret_cast<uint8_t*>(mem);
   stack += sizeof(Metadata);
+  unwindstack::Regs* regs = CreateFromRawData(metadata->arch, metadata->regs);
+	unwindstack::RemoteMaps maps(metadata->pid);
+	std::shared_ptr<unwindstack::Memory> mems = std::make_shared<StackMemory>(metadata->pid, reinterpret_cast<uint64_t>(metadata->sp), stack, metadata->size);
+	unwindstack::Unwinder unwinder(1000, &maps, regs, mems);
+	unwinder.Unwind();
+	total_frames += unwinder.NumFrames();
+	PERFETTO_LOG("Total frames: %d", total_frames);
   munmap(mem, sz);
-  close(fd);
 }
+
+class RecordReader {
+ public:
+	RecordReader() { Reset(); }
+
+	ssize_t Read(int fd) {
+		if (read_idx_ < sizeof(record_size_)) {
+			ssize_t rd = ReadRecordSize(fd);
+			if (rd != -1) read_idx_ += rd;
+			return rd;
+		} else {
+		 ssize_t rd = ReadRecord(fd);
+		 if (rd != -1) read_idx_ += rd;
+		 if (read_idx_ == record_size_) {
+			 Done(*outfd_, record_size_);
+			 Reset();
+		 }
+		 return rd;
+		}
+	}
+
+	void Reset() {
+		outfd_.reset(static_cast<int>(syscall(__NR_memfd_create, "data", 0)));
+		read_idx_ = 0;
+	}
+
+
+	bool done() {
+		return read_idx_ > sizeof(record_size_) && read_idx_ == record_size_;
+	}
+
+	int outfd() {
+		return *outfd_;
+	}
+
+ private:
+	ssize_t ReadRecordSize(int fd) {
+		ssize_t rd = PERFETTO_EINTR(read(fd, reinterpret_cast<uint8_t*>(&record_size_) + read_idx_, sizeof(record_size_) - read_idx_));
+		PERFETTO_CHECK(rd != -1 || errno == EAGAIN || errno == EWOULDBLOCK);
+		PERFETTO_LOG("Record size %d %" PRIu64, rd, record_size_);
+		return rd;
+	}
+
+	ssize_t ReadRecord(int fd) {
+		static uint64_t chunk_size = 16u * 4096u;
+		ssize_t rd = PERFETTO_EINTR(
+			splice(fd, nullptr, *outfd_, nullptr, std::min(chunk_size, record_size_ - read_idx_), SPLICE_F_NONBLOCK));
+		PERFETTO_CHECK(rd != -1 || errno == EAGAIN || errno == EWOULDBLOCK);
+		return rd;
+	}
+
+	base::ScopedFile outfd_;
+	uint8_t read_idx_;
+	uint64_t record_size_;
+};
+
 
 class PipeSender : public ipc::UnixSocket::EventListener {
  public:
@@ -82,31 +209,29 @@ void PipeSender::OnNewIncomingConnection(
   ipc::UnixSocket* p = new_connection.get();
   socks_.emplace(p, std::move(new_connection));
   int fd = pipes[0];
-  int outfd = static_cast<int>(syscall(__NR_memfd_create, "data", 0));
-  size_t read_bytes = 0;
+	// We do not want to block the event loop in case of
+	// spurious wake-ups.
   fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
   base::WeakPtr<PipeSender> weak_this = weak_factory_.GetWeakPtr();
+	RecordReader* record_reader = new RecordReader();
   task_runner_->AddFileDescriptorWatch(
-      fd, [fd, outfd, weak_this, read_bytes]() mutable {
+      fd, [fd, weak_this, record_reader]() mutable {
         if (!weak_this) {
           close(fd);
-          close(outfd);
+					delete record_reader;
           return;
         }
 
-        long rd = PERFETTO_EINTR(
-            splice(fd, nullptr, outfd, nullptr, 16 * 4096, SPLICE_F_NONBLOCK));
-        PERFETTO_CHECK(rd != -1 || errno == EAGAIN || errno == EWOULDBLOCK);
+        ssize_t rd = record_reader->Read(fd);
         if (rd == -1)
           return;
-        read_bytes += static_cast<size_t>(rd);
         total_read += static_cast<size_t>(rd);
         if ((total_read / 10000000) != ((total_read - rd) / 10000000))
           PERFETTO_LOG("perfhd: %lu\n", total_read);
         if (rd == 0) {
           weak_this->task_runner_->RemoveFileDescriptorWatch(fd);
           close(fd);
-          Done(outfd, read_bytes);
+					delete record_reader;
         }
       });
 }
