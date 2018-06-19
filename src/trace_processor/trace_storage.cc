b3d8fa3f1d77ef67f3ce5533325f49c6c6231e54
Lalit Maganti <lalitm@google.com>
2018-06-19 11:23:21 +0100

    [Patchset 2] trace_processor: don't add swapper slices to trace storage
    These slices add a lot of data that we don't really care about.
    
    Also fix counting of mismatched code when the previous event is not
    valid.
    
    Bug: 110195632
    Change-Id: Ie9b2c417348c25caae79529941f3303e0fee5fca

--- src/trace_processor/trace_storage.cc
+++ src/trace_processor/trace_storage.cc
@@ -39,22 +39,15 @@ void TraceStorage::PushSchedSwitch(uint32_t cpu,
 
   // If we had a valid previous event, then inform the storage about the
   // slice.
-  if (prev->valid()) {
+  if (prev->valid() && prev->next_pid != 0 /* Idle process (swapper/N) */) {
     uint64_t duration = timestamp - prev->timestamp;
     cpu_events_[cpu].AddSlice(prev->timestamp, duration, prev->prev_thread_id);
-
-    // If the this events previous pid does not match the previous event's next
-    // pid, make a note of this.
-    if (prev_pid != prev->next_pid) {
-      stats_.mismatched_sched_switch_tids_++;
-    }
   }
 
-  // We are switching into the swapper (i.e. idle) process. Reset the prev
-  // event so we do not add a slice for the time spent in idle.
-  if (next_pid == 0) {
-    *prev = {};
-    return;
+  // If the this events previous pid does not match the previous event's next
+  // pid, make a note of this.
+  if (prev_pid != prev->next_pid) {
+    stats_.mismatched_sched_switch_tids_++;
   }
 
   // Update the map with the current event.
