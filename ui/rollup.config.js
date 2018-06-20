ca4aa3877990c1715a96b8cf042d70a387253f54
Hector Dearman <hjd@google.com>
2018-06-20 15:19:39 +0100

    [Patchset 1] Add trace_processor bundle
    We had to shave a couple of yaks to get here:
    
    - Add -s MODULARIZE=1 to emscripten flags:
      This causes emscripten to generate a slighty more sane interface
      (where you can pass in Module rather than just adding to window).
    
    - In order for typescript to import trace_processor.js we need to copy
      it to out/blah/obj/ui/gen (which is symlinked to ui/src/gen). Hence
      a new target named wasm_gen.
    
    - However this means we need a .d.ts file for the generated
      trace_processor.js. We would normally just commit this but it needs
      to live in ui/src/gen (next to trace_processor.js) and ui/src/gen
      is a symlink to the output directory. So instead we add and extra
      output to wasm_lib to copy gn/standalone/wasm_typescript_declaration.d.ts
      next to each wasm_lib.js
    
    - trace_processor.js conditionally executes require('fs') (when running
      under node) this confuses rollup so we need to teach it not to worry
      about require('fs') (and similar requires).
    
    Change-Id: I17d10a9e2b46449b34f72e189116e67c9919b3fe

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,10 +1,22 @@
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
+// emscripten conditionally executes require('fs') and require('path'),
+// when running under node, rollup can't find a library named 'fs' or 'path'
+// so expects these to be present in the global scope (which fails at runtime).
+// To avoid this we ignore require('fs') and require('path').
+
 export default {
-  moduleName: 'perfetto',
+  output: {
+    name: 'perfetto'
+  },
   plugins: [
     nodeResolve(),
-    commonjs()
+    commonjs({
+      ignore: [
+        'fs',
+        'path'
+      ]
+    })
   ]
 }
