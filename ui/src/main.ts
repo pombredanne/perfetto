5591fc1b44679d2020dc8bd5d338f310c2b7efa8
Deepanjan Roy <dproy@google.com>
2018-07-03 18:48:24 +0100

    [Patchset 2] Clang-format typescript
    Clang format all the typescript files, and add a presubmit. The
    typescript can now be formatted using git cl format --js.
    
    There is one clang-format configuration that is different from Google
    style (see ui/.clang-format): BinPackArguments: false. These screenshots
    explain why:
    
    BinPackArguments: true - https://screenshot.googleplex.com/XqO41cJ5477
    BinPackArguments: false - https://screenshot.googleplex.com/rHHH1Y801Tm
    
    Bug:b/111101465
    Change-Id: I47f39ecb23fea69dc15ff6cac16fc214c3179877

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -15,6 +15,12 @@
  */
 
 import * as m from 'mithril';
+
+import {Engine} from './engine';
+import {
+  warmupWasmEngineWorker,
+  WasmEngineProxy,
+} from './engine/wasm_engine_proxy';
 import {frontend} from './frontend';
 
 console.log('Hello from the main thread!');
@@ -34,12 +40,33 @@ function createFrontend() {
   }
   const rect = root.getBoundingClientRect();
 
-  m.render(root, m(frontend, {width: rect.width, height: rect.height}));
+  m.render(root, m(frontend, {
+             width: rect.width,
+             height: rect.height,
+           }));
 }
 
-function main() {
+function main(input: Element, button: Element) {
   createController();
   createFrontend();
-}
 
-main();
+  warmupWasmEngineWorker();
+  // tslint:disable-next-line:no-any
+  input.addEventListener('change', (e: any) => {
+    const blob: Blob = e.target.files.item(0);
+    if (blob === null) return;
+    const engine: Engine = WasmEngineProxy.create(blob);
+    button.addEventListener('click', () => {
+      engine
+          .rawQuery({
+            sqlQuery: 'select * from sched;',
+          })
+          .then(result => console.log(result));
+    });
+  });
+}
+const input = document.querySelector('#trace');
+const button = document.querySelector('#query');
+if (input && button) {
+  main(input, button);
+}
