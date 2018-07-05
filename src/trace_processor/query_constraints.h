beb28207c739bbad186f07d013ab6b0cbaa11dd3
Isabelle Taylor <taylori@google.com>
2018-07-05 14:45:41 +0100

    trace_processor: Handling of sqlite string conversion
    A class to convert constraints to and from the string that sqlite form
    that sqlite needs them in.
    
    paired with: lalitm
    
    Bug:80416541
    Change-Id: I3f818ff31a7256418bb425ea99e4541ef9779fdf

---
+++ src/trace_processor/query_constraints.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_TRACE_PROCESSOR_QUERY_CONSTRAINTS_H_
+#define SRC_TRACE_PROCESSOR_QUERY_CONSTRAINTS_H_
+
+#include <vector>
+#include "sqlite3.h"
+
+namespace perfetto {
+namespace trace_processor {
+// This class stores the constraints (including the order by information) for
+// a query on a sqlite3 virtual table.
+// The constraints must be converted to and from a const char* to be used
+// by sqlite.
+class QueryConstraints {
+ public:
+  using Constraint = sqlite3_index_info::sqlite3_index_constraint;
+
+  struct OrderBy {
+    int column = 0;
+    bool desc = false;
+  };
+
+  void AddConstraint(int column, unsigned char op) {
+    Constraint c;
+    c.iColumn = column;
+    c.op = op;
+    constraints_.emplace_back(c);
+  }
+
+  void AddOrderBy(int column, bool desc) {
+    OrderBy ob;
+    ob.column = column;
+    ob.desc = desc;
+    order_by_.emplace_back(ob);
+  }
+  // Converts the constraints and order by information to a string for
+  // use by sqlite.
+  const char* ToNewSqlite3String();
+  static QueryConstraints FromString(const char* encoded_string);
+
+  const std::vector<OrderBy>& order_by() { return order_by_; }
+
+  const std::vector<Constraint>& constraints() { return constraints_; }
+
+ private:
+  std::vector<OrderBy> order_by_;
+  std::vector<Constraint> constraints_;
+};
+
+}  // namespace trace_processor
+}  // namespace perfetto
+
+#endif  // SRC_TRACE_PROCESSOR_QUERY_CONSTRAINTS_H_
