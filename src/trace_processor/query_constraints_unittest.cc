1e61947ec513d49f98008906f81ffd79792780d9
Isabelle Taylor <taylori@google.com>
2018-07-09 12:27:44 +0100

    [Patchset 6] trace_processor: thread table implementation
    Fast filtering on Utid. Can be joined with the process table on Upid.
    
    Also a few small changes to process table to be consistent with the
    thread table.
    
    Bug:80416541
    Change-Id: Ie97f1997fef28d6af250aa8199b59aeebb5af8f1

--- src/trace_processor/query_constraints_unittest.cc
+++ src/trace_processor/query_constraints_unittest.cc
@@ -15,9 +15,15 @@
  */
 
 #include "src/trace_processor/query_constraints.h"
-
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "perfetto/base/logging.h"
+
+using testing::ElementsAreArray;
+using testing::Matcher;
+using testing::Field;
+using testing::Pointwise;
+using testing::Matches;
 
 namespace perfetto {
 namespace trace_processor {
@@ -26,6 +32,14 @@ namespace {
 TEST(QueryConstraintsTest, ConvertToAndFromSqlString) {
   QueryConstraints qc;
   qc.AddConstraint(12, 0);
+
+  QueryConstraints::SqliteString only_constraint = qc.ToNewSqlite3String();
+  ASSERT_TRUE(strcmp(only_constraint.get(), "C1,12,0,O0") == 0);
+
+  QueryConstraints qc_constraint =
+      QueryConstraints::FromString(only_constraint.get());
+  ASSERT_EQ(qc, qc_constraint);
+
   qc.AddOrderBy(1, false);
   qc.AddOrderBy(21, true);
 
@@ -33,16 +47,7 @@ TEST(QueryConstraintsTest, ConvertToAndFromSqlString) {
   ASSERT_TRUE(strcmp(result.get(), "C1,12,0,O2,1,0,21,1") == 0);
 
   QueryConstraints qc_result = QueryConstraints::FromString(result.get());
-
-  for (size_t i = 0; i < qc.constraints().size(); i++) {
-    ASSERT_EQ(qc.constraints()[i].iColumn, qc_result.constraints()[i].iColumn);
-    ASSERT_EQ(qc.constraints()[i].op, qc_result.constraints()[i].op);
-  }
-
-  for (size_t i = 0; i < qc.order_by().size(); i++) {
-    ASSERT_EQ(qc.order_by()[i].iColumn, qc_result.order_by()[i].iColumn);
-    ASSERT_EQ(qc.order_by()[i].desc, qc_result.order_by()[i].desc);
-  }
+  ASSERT_EQ(qc, qc_result);
 }
 
 TEST(QueryConstraintsTest, CheckEmptyConstraints) {
@@ -55,6 +60,18 @@ TEST(QueryConstraintsTest, CheckEmptyConstraints) {
       QueryConstraints::FromString(string_result.get());
   ASSERT_EQ(qc_result.constraints().size(), 0);
   ASSERT_EQ(qc_result.order_by().size(), 0);
+}
+
+TEST(QueryConstraintsTest, OnlyOrderBy) {
+  QueryConstraints qc;
+  qc.AddOrderBy(3, true);
+
+  QueryConstraints::SqliteString string_result = qc.ToNewSqlite3String();
+  ASSERT_TRUE(strcmp(string_result.get(), "C0,O1,3,1") == 0);
+
+  QueryConstraints qc_result =
+      QueryConstraints::FromString(string_result.get());
+  ASSERT_EQ(qc, qc_result);
 }
 
 }  // namespace
