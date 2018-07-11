0736166433260a5202be015996eff325eef80637
Deepanjan Roy <dproy@google.com>
2018-07-11 09:46:52 -0400

    [Patchset 2] Move node_modules install to install-build-deps
    Also check that tslint exists before running presubmit.
    
    Change-Id: I358edb82ca5dfcee39c0379f79a987df749f0565

--- ui/PRESUBMIT.py
+++ ui/PRESUBMIT.py
@@ -39,8 +39,10 @@ def CheckTslint(input_api, output_api):
     if not path.exists(tslint):
         repo_root = input_api.change.RepositoryRoot()
         install_path = path.join(repo_root, 'tools', 'install-build-deps')
-        return output_api.PresubmitError(
+        return [
+            output_api.PresubmitError(
                 "Tslint not found. Please run {0} --ui".format(install_path))
+        ]
 
     # Some tslint rules require type information and thus need the whole
     # project. We therefore call tslint on the whole project instead of only the
