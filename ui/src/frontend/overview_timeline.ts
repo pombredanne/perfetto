0218ed5bf8e0fc838ffd68374b9f0361fd52baad
Michail Schwab <michaschwab@google.com>
2018-08-02 13:20:38 -0400

    [Patchset 2] Allow Brushing Everywhere Instead of Only Via Handles
    So far, brushing only works by dragging handles around. With this CL,
    arbirary regions can be selected directly.
    
    Change-Id: I9442b4451e5032af742e5353219033b1548f6810

--- ui/src/frontend/overview_timeline.ts
+++ ui/src/frontend/overview_timeline.ts
@@ -91,40 +91,44 @@ export const OverviewTimeline = {
  */
 const HorizontalBrushSelection = {
   oninit() {
-    let dragState: 'start'|'end'|'none' = 'none';
+    let dragState: 'draggingStartHandle'|'draggingEndHandle'|'notDragging' =
+        'notDragging';
 
     this.rightHandleMouseDownListener = () => {
-      dragState = 'end';
+      dragState = 'draggingEndHandle';
     };
     this.leftHandleMouseDownListener = () => {
-      dragState = 'start';
+      dragState = 'draggingStartHandle';
     };
     this.mouseDownListener = (e: MouseEvent) => {
       const posX = e.clientX - this.offsetLeft;
-      dragState = 'end';
+      dragState = 'draggingEndHandle';
       this.onBrushedPx(posX, posX + 1);
     };
     this.mouseMoveListener = (e: MouseEvent) => {
-      if (dragState === 'none') {
+      if (dragState === 'notDragging') {
         return;
       }
       // Prevent text selections
       e.preventDefault();
 
       const posX = e.clientX - this.offsetLeft;
-      if ((dragState === 'end' && posX < this.selectionPx.start) ||
-          (dragState === 'start' && posX > this.selectionPx.end)) {
+      if ((dragState === 'draggingEndHandle' &&
+           posX < this.selectionPx.start) ||
+          (dragState === 'draggingStartHandle' &&
+           posX > this.selectionPx.end)) {
         // Flip start and end if handle has been dragged past the other limit.
-        dragState = dragState === 'start' ? 'end' : 'start';
+        dragState = dragState === 'draggingStartHandle' ? 'draggingEndHandle' :
+                                                          'draggingStartHandle';
       }
-      if (dragState === 'start') {
+      if (dragState === 'draggingStartHandle') {
         this.onBrushedPx(posX, this.selectionPx.end);
       } else {
         this.onBrushedPx(this.selectionPx.start, posX);
       }
     };
     this.mouseUpListener = () => {
-      dragState = 'none';
+      dragState = 'notDragging';
     };
   },
   oncreate(vnode) {
