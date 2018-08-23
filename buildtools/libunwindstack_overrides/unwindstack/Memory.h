3d31699689a9640d09c8fda2419cc2a2bbef513b
Florian Mayer <fmayer@google.com>
2018-08-23 10:09:15 -0700

    [Patchset 8] profiling: Add parts of heap profiler.
    Change-Id: Ibece519dbddb6dac06ccc4103fd9b1b2b1e559e5

---
+++ buildtools/libunwindstack_overrides/unwindstack/Memory.h
@@ -0,0 +1,22 @@
+#ifndef BUILDTOOLS_LIBUNWINDSTACK_OVERRIDES_UNWINDSTACK_MEMORY_H_
+#define BUILDTOOLS_LIBUNWINDSTACK_OVERRIDES_UNWINDSTACK_MEMORY_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/uio.h>
+
+#include "../../android-core/libunwindstack/include/unwindstack/Memory.h"
+
+#if __ANDROID_API__ < 26
+// Android API level 21, which Perfetto builds as currently, does not
+// expose this function. libunwindstack depends on it, but we do not
+// use that functionality.
+extern ssize_t process_vm_readv(pid_t pid,
+                                const struct iovec* local_iov,
+                                unsigned long liovcnt,
+                                const struct iovec* remote_iov,
+                                unsigned long riovcnt,
+                                unsigned long flags);
+#endif
+
+#endif  // BUILDTOOLS_LIBUNWINDSTACK_OVERRIDES_UNWINDSTACK_MEMORY_H_
