1c4e723556d7e666ecb65f1b9d617c3831981edd
Florian Mayer <fmayer@google.com>
2018-08-30 15:56:49 -0700

    [Patchset 6] Do not run CTS on  64bit kernel and 32bit userland
    Bug: 110495592
    Change-Id: I5c21b3d258969491ec042b635ebac94e6dca8785

--- test/end_to_end_integrationtest.cc
+++ test/end_to_end_integrationtest.cc
@@ -59,17 +59,15 @@ namespace perfetto {
 #define MAYBE_TestFtraceProducer DISABLED_TestFtraceProducer
 #endif
 TEST(PerfettoTest, MAYBE_TestFtraceProducer) {
-#if PERFETTO_BUILDFLAG(PERFETTO_ANDROID_BUILD)
   // Parsing ftrace is broken in Android Pie when running a 64 bit kernel
   // and a 32 bit userland or other way round. Exclude those.
   struct utsname un;
   PERFETTO_CHECK(uname(&un) != -1);
   size_t kernel_size = strstr(un.machine, "64") ? 8 : 4;
-  if (sizeof(void*) != kernel_sizboole) {
+  if (sizeof(void*) != kernel_size) {
     // Kernel is 64bit and userspace is 32bit or other way round.
     return;
   }
-#endif
 
   base::TestTaskRunner task_runner;
 
