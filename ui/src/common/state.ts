f14314df001ae48421cf4f0936f28aa98f535dc4
Hector Dearman <hjd@google.com>
2018-09-18 11:47:00 +0100

    [Patchset 1] perfetto-ui: Make track config generic
    Previously Track specific config (cpu number, upid, utid etc) was
    directly part of TrackState. This was unfotunate as it required
    introducing a new track to edit TrackState (and reducer.ts and
    actions.ts). The CL introduces a 'config' field on TrackState which
    each track type can fill with its own custom data. To support this
    both Track and TrackController now take an optional type parameter:
    Config which determines the type of this field.
    
    Also removes AddChromeTrack and makes AddTrack not take a cpu argument.
    
    Change-Id: Ideabf3496c796579bda0fbc0eabb18fb08fa6ec3

--- ui/src/common/state.ts
+++ ui/src/common/state.ts
@@ -22,16 +22,15 @@ export interface ObjectById<Class extends{id: string}> { [id: string]: Class; }
 export interface TrackState {
   id: string;
   engineId: string;
-  maxDepth: number;
   kind: string;
   name: string;
-  // TODO: These need to be nested into track kind spesific state.
-  // cpu slice state:
-  cpu: number;
-  // chrome slice state:
-  upid?: number;
-  utid?: number;
+
   dataReq?: TrackDataRequest;
+  config: {};
+
+  //// TODO: These need to be nested into track kind spesific state.
+  //// cpu slice state:
+  // cpu: number;
 }
 
 export interface TrackDataRequest {
