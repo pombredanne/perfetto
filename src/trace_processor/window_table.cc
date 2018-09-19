af29ef260be814cdc500664977ee25edf79e105c
Lalit Maganti <lalitm@google.com>
2018-09-19 14:13:54 +0100

    [Patchset 1] trace_processor: add window virtual table
    This allows the UI to specify the window duration to SPAN JOIN against.
    
    Change-Id: I22340a558502161d47fe1b3dc34593cd14156990

---
+++ src/trace_processor/window_table.cc
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/trace_processor/window_table.h"
+
+#include "src/trace_processor/sqlite_utils.h"
+
+namespace perfetto {
+namespace trace_processor {
+
+namespace {
+using namespace sqlite_utils;
+}  // namespace
+
+WindowTable::WindowTable(sqlite3*, const TraceStorage*) {}
+
+void WindowTable::RegisterTable(sqlite3* db, const TraceStorage* storage) {
+  Table::Register<WindowTable>(db, storage, "window", true);
+}
+
+std::string WindowTable::CreateTableStmt(int, const char* const*) {
+  // Note: ts is not strictly a primary key since multiple CPUs can have the
+  // same ts. However, SQL would prevent us from having xUpdate method if we
+  // specified more than one column on a WITHOUT ROWID table.
+  return "CREATE TABLE x("
+         "quantum HIDDEN UNSIGNED BIG INT, "
+         "window_start HIDDEN UNSIGNED BIG INT, "
+         "window_dur HIDDEN UNSIGNED BIG INT, "
+         "ts UNSIGNED BIG INT, "
+         "dur UNSIGNED BIG INT, "
+         "cpu UNSIGNED INT, "
+         "quantized_group UNSIGNED BIG INT, "
+         "rowid UNSIGNED BIG INT, "
+         "PRIMARY KEY(rowid)"
+         ") WITHOUT ROWID;";
+}
+
+std::unique_ptr<Table::Cursor> WindowTable::CreateCursor() {
+  uint64_t window_end = window_start_ + window_dur_;
+  uint64_t step_size = quantum_ == 0 ? window_dur_ : quantum_;
+  return std::unique_ptr<Table::Cursor>(
+      new Cursor(this, window_start_, window_end, step_size));
+}
+
+int WindowTable::BestIndex(const QueryConstraints&, BestIndexInfo*) {
+  return SQLITE_OK;
+}
+
+int WindowTable::Update(int argc, sqlite3_value** argv, sqlite3_int64*) {
+  // We only support updates to ts and dur. Disallow deletes (argc == 1) and
+  // inserts (argv[0] == null).
+  if (argc < 2 || sqlite3_value_type(argv[0]) == SQLITE_NULL)
+    return SQLITE_READONLY;
+
+  quantum_ = static_cast<uint64_t>(sqlite3_value_int64(argv[2]));
+  window_start_ = static_cast<uint64_t>(sqlite3_value_int64(argv[3]));
+  window_dur_ = static_cast<uint64_t>(sqlite3_value_int64(argv[4]));
+
+  return SQLITE_OK;
+}
+
+WindowTable::Cursor::Cursor(const WindowTable* table,
+                            uint64_t window_start,
+                            uint64_t window_end,
+                            uint64_t step_size)
+    : window_start_(window_start),
+      window_end_(window_end),
+      step_size_(step_size),
+      table_(table) {}
+
+int WindowTable::Cursor::Column(sqlite3_context* context, int N) {
+  switch (N) {
+    case Column::kQuantum: {
+      sqlite3_result_int64(context,
+                           static_cast<sqlite_int64>(table_->quantum_));
+      break;
+    }
+    case Column::kWindowStart: {
+      sqlite3_result_int64(context,
+                           static_cast<sqlite_int64>(table_->window_start_));
+      break;
+    }
+    case Column::kWindowDur: {
+      sqlite3_result_int(context, static_cast<int>(table_->window_dur_));
+      break;
+    }
+    case Column::kTs: {
+      sqlite3_result_int64(context, static_cast<sqlite_int64>(current_ts_));
+      break;
+    }
+    case Column::kDuration: {
+      sqlite3_result_int64(context, static_cast<sqlite_int64>(step_size_));
+      break;
+    }
+    case Column::kCpu: {
+      sqlite3_result_int(context, static_cast<int>(current_cpu_));
+      break;
+    }
+    case Column::kQuantizedGroup: {
+      sqlite3_result_int64(context,
+                           static_cast<sqlite_int64>(quantized_group_));
+      break;
+    }
+    case Column::kRowId: {
+      sqlite3_result_int64(context, static_cast<sqlite_int64>(row_id_));
+      break;
+    }
+    default: {
+      PERFETTO_FATAL("Unknown column %d", N);
+      break;
+    }
+  }
+  return SQLITE_OK;
+}
+
+int WindowTable::Cursor::Filter(const QueryConstraints& qc, sqlite3_value** v) {
+  current_ts_ = window_start_;
+  current_cpu_ = 0;
+  quantized_group_ = 0;
+  row_id_ = 0;
+  return_first = qc.constraints().size() == 1 &&
+                 qc.constraints()[0].iColumn == Column::kRowId &&
+                 sqlite3_value_int(v[0]) == 0;
+  return SQLITE_OK;
+}
+
+int WindowTable::Cursor::Next() {
+  // If we're only returning the first row, set the values to EOF.
+  if (return_first) {
+    current_cpu_ = base::kMaxCpus;
+    current_ts_ = window_end_;
+    return SQLITE_OK;
+  }
+
+  if (++current_cpu_ == base::kMaxCpus && current_ts_ < window_end_) {
+    current_cpu_ = 0;
+    current_ts_ += step_size_;
+    quantized_group_++;
+  }
+  row_id_++;
+  return SQLITE_OK;
+}
+
+int WindowTable::Cursor::Eof() {
+  return current_cpu_ == base::kMaxCpus && current_ts_ >= window_end_;
+}
+
+}  // namespace trace_processor
+}  // namespace perfetto
---
+++ src/trace_processor/window_table.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_TRACE_PROCESSOR_WINDOW_TABLE_H_
+#define SRC_TRACE_PROCESSOR_WINDOW_TABLE_H_
+
+#include <limits>
+#include <memory>
+
+#include "src/trace_processor/table.h"
+#include "src/trace_processor/trace_storage.h"
+
+namespace perfetto {
+namespace trace_processor {
+
+// The implementation of the SQLite table containing each unique process with
+// the metadata for those processes.
+class WindowTable : public Table {
+ public:
+  enum Column {
+    kQuantum = 0,
+    kWindowStart = 1,
+    kWindowDur = 2,
+    kTs = 3,
+    kDuration = 4,
+    kCpu = 5,
+    kQuantizedGroup = 6,
+    kRowId = 7
+  };
+
+  static void RegisterTable(sqlite3* db, const TraceStorage* storage);
+
+  WindowTable(sqlite3*, const TraceStorage*);
+
+  // Table implementation.
+  std::string CreateTableStmt(int argc, const char* const* argv) override;
+  std::unique_ptr<Table::Cursor> CreateCursor() override;
+  int BestIndex(const QueryConstraints&, BestIndexInfo*) override;
+  int Update(int, sqlite3_value**, sqlite3_int64*) override;
+
+ private:
+  class Cursor : public Table::Cursor {
+   public:
+    Cursor(const WindowTable*,
+           uint64_t window_start,
+           uint64_t window_end,
+           uint64_t step_size);
+
+    // Implementation of Table::Cursor.
+    int Filter(const QueryConstraints&, sqlite3_value**) override;
+    int Next() override;
+    int Eof() override;
+    int Column(sqlite3_context*, int N) override;
+
+   private:
+    uint64_t const window_start_;
+    uint64_t const window_end_;
+    uint64_t const step_size_;
+    const WindowTable* const table_;
+
+    uint64_t current_ts_ = 0;
+    uint32_t current_cpu_ = 0;
+    uint64_t quantized_group_ = 0;
+    uint64_t row_id_ = 0;
+    bool return_first = false;
+  };
+
+  uint64_t quantum_ = 0;
+  uint64_t window_start_ = 0;
+  uint64_t window_dur_ = std::numeric_limits<uint64_t>::max();
+};
+}  // namespace trace_processor
+}  // namespace perfetto
+
+#endif  // SRC_TRACE_PROCESSOR_WINDOW_TABLE_H_
