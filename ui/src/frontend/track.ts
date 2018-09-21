92cb8acff1c6795800def38503f8f1ab9f008b08
Lalit Maganti <lalitm@google.com>
2018-09-21 11:27:00 +0100

    [Patchset 19] trace_processor: add the SPAN JOIN virtual table
    This table implments the SPAN JOIN concept implemented in go/dctv-db-design
    which is an elegant way to define time series composition between two
    tables.
    
    For example, we will use SPAN JOIN to compute cycles by joining the
    sched slice table with the frequency table.
    
    Change-Id: I8dedd56e84e9e27a1bda8574126fd5eb5ff9fbca

--- ui/src/frontend/track.ts
+++ ui/src/frontend/track.ts
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import {TrackState} from '../common/state';
+import {globals} from './globals';
 
 /**
  * This interface forces track implementations to have some static properties.
@@ -32,15 +33,26 @@ export interface TrackCreator {
 /**
  * The abstract class that needs to be implemented by all tracks.
  */
-export abstract class Track {
+export abstract class Track<Config = {}, Data = {}> {
   /**
    * Receive data published by the TrackController of this track.
    */
   constructor(protected trackState: TrackState) {}
   abstract renderCanvas(ctx: CanvasRenderingContext2D): void;
+
+  get config(): Config {
+    return this.trackState.config as Config;
+  }
+
+  data(): Data {
+    return globals.trackDataStore.get(this.trackState.id) as Data;
+  }
+
   getHeight(): number {
     return 40;
   }
+
   onMouseMove(_position: {x: number, y: number}) {}
+
   onMouseOut() {}
 }
