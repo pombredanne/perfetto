2b4ce62cc14fec1ad5b773516c8bc45250afb814
Lalit Maganti <lalitm@google.com>
2018-09-26 09:14:49 +0100

    [Patchset 1] trace_processor: make schema more explicit in tables
    This allows for the create table statement to be generated in the table subclass
    and will eventually allow us to share code among cursors (in an upcoming
    CL).
    
    Change-Id: I324e6779a43c255a29dc8c6a08e792f3d61bc13f

--- src/trace_processor/sched_slice_table.cc
+++ src/trace_processor/sched_slice_table.cc
@@ -96,14 +96,15 @@ void SchedSliceTable::RegisterTable(sqlite3* db, const TraceStorage* storage) {
   Table::Register<SchedSliceTable>(db, storage, "sched");
 }
 
-std::string SchedSliceTable::CreateTableStmt(int, const char* const*) {
-  return "CREATE TABLE sched("
-         "ts UNSIGNED BIG INT, "
-         "cpu UNSIGNED INT, "
-         "dur UNSIGNED BIG INT, "
-         "utid UNSIGNED INT, "
-         "PRIMARY KEY(cpu, ts)"
-         ") WITHOUT ROWID;";
+Table::Schema SchedSliceTable::CreateSchema(int, const char* const*) {
+  return Schema(
+      {
+          Table::Column("ts", ColumnType::kUlong),
+          Table::Column("cpu", ColumnType::kUint),
+          Table::Column("dur", ColumnType::kUlong),
+          Table::Column("utid", ColumnType::kUint),
+      },
+      {Column::kCpu, Column::kTimestamp});
 }
 
 std::unique_ptr<Table::Cursor> SchedSliceTable::CreateCursor() {
--- src/trace_processor/thread_table.cc
+++ src/trace_processor/thread_table.cc
@@ -36,14 +36,15 @@ void ThreadTable::RegisterTable(sqlite3* db, const TraceStorage* storage) {
   Table::Register<ThreadTable>(db, storage, "thread");
 }
 
-std::string ThreadTable::CreateTableStmt(int, const char* const*) {
-  return "CREATE TABLE x("
-         "utid UNSIGNED INT, "
-         "upid UNSIGNED INT, "
-         "name TEXT, "
-         "tid UNSIGNED INT, "
-         "PRIMARY KEY(utid)"
-         ") WITHOUT ROWID;";
+Table::Schema ThreadTable::CreateSchema(int, const char* const*) {
+  return Schema(
+      {
+          Table::Column("utid", ColumnType::kInt),
+          Table::Column("upid", ColumnType::kInt),
+          Table::Column("name", ColumnType::kString),
+          Table::Column("tid", ColumnType::kInt),
+      },
+      {Column::kUtid});
 }
 
 std::unique_ptr<Table::Cursor> ThreadTable::CreateCursor() {
