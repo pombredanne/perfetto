11c72e71f9d99a6a3a69dea05d795f5eb7624982
Florian Mayer <fmayer@google.com>
2018-09-26 17:23:04 +0100

    [Patchset 8] Move unix socket to base.
    Change-Id: Ic84986efb90ee95a45abc4558fa472f890acd885

--- src/profiling/memory/unwinding.h
+++ src/profiling/memory/unwinding.h
@@ -80,8 +80,8 @@ struct UnwindingRecord {
 };
 
 struct FreeRecord {
-  size_t size;
   std::unique_ptr<uint8_t[]> free_data;
+  FreeMetadata* metadata;
 };
 
 struct AllocRecord {
@@ -96,10 +96,7 @@ struct BookkeepingRecord {
   FreeRecord free_record;
 };
 
-bool DoUnwind(void* mem,
-              size_t sz,
-              ProcessMetadata* metadata,
-              AllocRecord* out);
+bool DoUnwind(WireMessage*, ProcessMetadata* metadata, AllocRecord* out);
 
 bool HandleUnwindingRecord(UnwindingRecord* rec, BookkeepingRecord* out);
 void HandleBookkeepingRecord(BookkeepingRecord* rec);
