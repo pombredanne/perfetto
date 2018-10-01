4ff23c92dff6a2ac2dcc2ce5990db1c0517938a8
Florian Mayer <fmayer@google.com>
2018-10-01 12:35:32 +0100

    [Patchset 2] Add unittest for wire_protocol
    Fix bug.
    
    Change-Id: Ide59ba95da4cc22970a25701a245516427e9c18e

--- src/profiling/memory/unwinding.cc
+++ src/profiling/memory/unwinding.cc
@@ -238,7 +238,7 @@ void HandleBookkeepingRecord(BookkeepingRecord* rec) {
     FreeRecord& free_rec = rec->free_record;
     FreePageEntry* entries = free_rec.metadata->entries;
     uint64_t num_entries = free_rec.metadata->num_entries;
-    if (num_entries >= kFreePageSize)
+    if (num_entries > kFreePageSize)
       return;
     for (size_t i = 0; i < num_entries; ++i) {
       const FreePageEntry& entry = entries[i];
