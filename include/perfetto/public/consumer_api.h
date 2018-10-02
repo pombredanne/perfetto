66109cbdc0cfabd95fea414b4c9c8e323b01ebde
Primiano Tucci <primiano@google.com>
2018-10-02 23:36:45 +0100

    [Patchset 2] Add public API for Perfetto consumer
    WORK IN PROGRESS
    An API that services in the Android tree can depend on.
    It relies on libtraced_shared.so.
    
    Bug: 116547201
    Change-Id: Ia8a03a5c8f8cbf3fa3aa893f169073646790d6cb

--- include/perfetto/public/consumer_api.h
+++ include/perfetto/public/consumer_api.h
@@ -21,14 +21,40 @@
 
 extern "C" {
 
+// Enables tracing with the given TraceConfig. If the trace config has the
+// |deferred_start| flag set (see trace_config.proto) tracing is initialized
+// but not started. An explicit call to StartTracing() must be issued in order
+// to start the capture.
+// The caller must take ownership of the returned pipe. The returned pipe serves
+// also to scope the lifetime of the tracing session.
+// Args:
+//   [trace_config_proto, trace_config_len] point to a binary-encoded proto
+//   containing the trace config. See //external/perfetto/docs/trace-config.md
+//   for more details.
+// Return value:
+//    Returns the reading end of a pipe that can be used to read the trace from
+//    any thread. The pipe will be closed (so reading it will return 0 -> EOF)
+//    when the trace ended or an error occured. Errors will be logged to stderr
+//    and logcat.
+//    The pipe can be read during the trace (streaming mode) or at the end. In
+//    the latter case, the pipe will be backed by the tracing userspace ring
+//    buffer(s).
+//    If the read end is closed before tracing ends, the tracing session will
+//    be terminated prematurely.
+int PerfettoConsumer_EnableTracing(const void* trace_config_proto,
+                                   size_t trace_config_len);
+
+// Starts recording the trace. Can be used only when setting the
+// |deferred_start| flag in the trace config passed to EnableTracing().
+// The estimated end-to-end (this call to ftrace enabling) latency is 2-3 ms
+// on a Pixel 2.
+void PerfettoConsumer_StartTracing();
+
+// TODO seems unnecessary here unless we want to expose a PollState() method.
 enum PerfettoConsumer_State {
   PerfettoConsumer_kDisconnected = 0,
   PerfettoConsumer_kEnabled = 0,
 };
-
-int PerfettoConsumer_EnableTracing(const void* trace_config_proto,
-                                   size_t trace_config_len);
-void PerfettoConsumer_StartTracing();
 
 }  // extern "C".
 
