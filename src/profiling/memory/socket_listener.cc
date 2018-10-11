94fe51e15c683854292832ef3465c727ed295d59
Florian Mayer <fmayer@google.com>
2018-10-11 17:23:37 +0100

    [Patchset 39] profiling: Allow to generate heap dumps.
    Fix ownership of HeapTracker. Before this change, there was the
    following problem: HeapTracker was owned by SocketListener (via
    SocketListener::Entry) and de-allocated when the last socket for a PID
    went away. There are two problems with this:
    
    * We access GlobalCallsitesTrie from two different threads, potentially
    racing and crashing.
    
    * We actually want to keep the data around until after we have dumped
    it, even if the last socket has already gone away.
    
    The current ownership semantics do not allow for that.
    
    Change-Id: I6514ea68f2da99913cbc07422ff68fa3d9b9c27e

--- src/profiling/memory/socket_listener.cc
+++ src/profiling/memory/socket_listener.cc
@@ -58,9 +58,9 @@ void SocketListener::OnDataAvailable(base::UnixSocket* self) {
       self->Send(&client_config_, sizeof(client_config_), -1,
                  base::UnixSocket::BlockingMode::kBlocking);
     } else if (fds[0] || fds[1]) {
-      PERFETTO_LOG("Received partial FDs.");
+      PERFETTO_DLOG("Received partial FDs.");
     } else {
-      PERFETTO_LOG("Received no FDs.");
+      PERFETTO_DLOG("Received no FDs.");
     }
   }
   RecordReader::Record record;
