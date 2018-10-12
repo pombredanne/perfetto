1d2f7f0235b3442d7da686a1dd6317cb6f07d3d8
Lalit Maganti <lalitm@google.com>
2018-10-12 11:14:00 +0100

    [Patchset 5] trace_processor: add diff testing for trace processor
    Trace processor is notoriously hard to test using unit or integration
    tests because of the intermediate steps being closely tied to the code
    and being implementation dependent.
    
    Fundamentally, all we really care about is that for a certain set of
    traces and queries, we maintain correctness and performance.
    
    This can be done using diff testing where we take a trace and a query
    and run the whole end-to-end trace processor over it, comparing the
    output to the expected (i.e. correct) output and complaining if that is
    not correct.
    
    This CL only addresses the correctness portion. The performance portion
    will be tackled in a follow-up.
    
    Bug: 117596113
    Change-Id: Idbfe1fc732bea9b3b7a926ef2d99d3c2ac90dc16

--- tools/diff_test_trace_processor.py
+++ tools/diff_test_trace_processor.py
@@ -21,46 +21,66 @@ import subprocess
 import sys
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEST_DATA_DIR = os.path.join(ROOT_DIR, "test", "data")
+TEST_DATA_DIR = os.path.join(ROOT_DIR, "test", "trace_processor")
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--traces', type=str, help='location of trace files',
-                      default=os.path.join(TEST_DATA_DIR, "traces"))
-  parser.add_argument('--queries', type=str, help='location of query files',
-                      default=os.path.join(TEST_DATA_DIR, "queries"))
-  parser.add_argument('--expected', type=str, help='location of expected files',
-                      default=os.path.join(TEST_DATA_DIR, "expected"))
+  parser.add_argument('--index', type=str, help='location of index file',
+                      default=os.path.join(TEST_DATA_DIR, "index"))
   parser.add_argument('trace_processor', type=str,
                       help='location of trace processor binary')
   args = parser.parse_args()
 
-  expected_files = glob.glob(os.path.join(args.expected, "*.out"))
+  with open(args.index, 'r') as file:
+    index_lines = file.readlines()
 
-  for f in expected_files:
-    filename_with_ext = os.path.basename(f)
-    (filename, _) = os.path.splitext(filename_with_ext)
+  test_failure = 0
+  index_dir = os.path.dirname(args.index)
+  for line in index_lines:
+    [trace_fname, query_fname, expected_fname] = line.strip().split(' ')
 
-    [trace_name, query_name] = filename.split('-')
-
-    trace_path = os.path.join(args.traces, trace_name + '.pb')
-    query_path = os.path.join(args.queries, query_name + '.sql')
-
-    if not os.path.exists(trace_path) or not os.path.exists(query_path):
-      print("Trace or query file not found for expected file {}".format(f))
+    trace_path = os.path.abspath(os.path.join(index_dir, trace_fname))
+    query_path = os.path.abspath(os.path.join(index_dir, query_fname))
+    expected_path = os.path.abspath(os.path.join(index_dir, expected_fname))
+    if not os.path.exists(trace_path):
+      print("Trace file not found {}".format(trace_path))
+      return 1
+    elif not os.path.exists(query_path):
+      print("Query file not found {}".format(query_path))
+      return 1
+    elif not os.path.exists(expected_path):
+      print("Expected file not found {}".format(expected_path))
       return 1
 
-    with open(query_path, "r") as query_file:
-      actual_raw = subprocess.check_output([args.trace_processor, trace_path],
-                                           stdin=query_file, stderr=None)
-      actual = actual_raw.decode("utf-8")
+    actual_raw = subprocess.check_output([
+      args.trace_processor,
+      '-q',
+      query_path,
+      trace_path
+    ])
+    actual = actual_raw.decode("utf-8")
+    actual_lines = actual_raw.splitlines(True)
 
-    with open(f, "r") as expected_file:
+    with open(expected_path, "r") as expected_file:
       expected = expected_file.read()
       if expected != actual:
-        print("Expected did not match actual for {}".format(f))
+        sys.stderr.write(
+          "Expected did not match actual for trace {} and query {}"
+          .format(trace_path, query_path))
 
-  return 0
+        expected_lines = expected.splitlines(True)
+        diff = difflib.unified_diff(expected_lines, actual_lines,
+                                    fromfile="expected", tofile="actual")
+        for line in diff:
+          sys.stderr.write(line)
+        test_failure += 1
+
+  if test_failure == 0:
+    print("All tests passed successfully")
+    return 0
+  else:
+    print("Total failures: {}".format(test_failure))
+    return 1
 
 if __name__ == '__main__':
   sys.exit(main())
