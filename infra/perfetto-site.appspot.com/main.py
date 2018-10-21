fe434844f650a981b5561c11a120c80e9b3e8b12
Primiano Tucci <primiano@google.com>
2018-10-21 04:27:14 +0100

    [Patchset 1] Docs: add docsify support
    Looks nicer
    
    Change-Id: I707772e47e3e8441f93af1e9ba14a4c266f080b3

--- infra/perfetto-site.appspot.com/main.py
+++ infra/perfetto-site.appspot.com/main.py
@@ -12,14 +12,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+
 import webapp2
 
-REDIRECT_URL = 'https://android.googlesource.com/platform/external/perfetto/+/master/README.md'
+BASE = 'https://catapult-project.github.io/perfetto/%s'
 
-class RedirectHandler(webapp2.RequestHandler):
-  def get(self):
-    self.redirect(REDIRECT_URL)
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        handler = GithubMirrorHandler()
+        handler.initialize(self.request, self.response)
+        return handler.get("index.html")
+
+
+class GithubMirrorHandler(webapp2.RequestHandler):
+    def get(self, resource):
+        if '..' in resource:
+          self.response.set_status(403)
+          return
+
+        url = BASE % resource
+        contents = memcache.get(url)
+        if not contents or self.request.get('reload'):
+            result = urlfetch.fetch(url)
+            if result.status_code != 200:
+                self.response.set_status(result.status_code)
+                self.response.write(result.content)
+                return
+            contents = result.content
+            memcache.set(url, contents, time=60*60*24)  # 1 day
+
+        self.response.headers['Content-Type'] = result.headers['Content-Type']
+        self.response.write(contents)
+
 
 app = webapp2.WSGIApplication([
-    ('/', RedirectHandler),
+    ('/', MainHandler),
+    ('/(.+)', GithubMirrorHandler),
 ], debug=True)
