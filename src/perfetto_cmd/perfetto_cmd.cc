9ef64c593cc887d7469ee8472c02d3f7902d54bb
Florian Mayer <fmayer@google.com>
2018-11-29 13:51:07 +0000

    [Patchset 1] Flush on SIGINT to perfetto cmd.
    Change-Id: I6b76ebfa24cc88a8477785c95cfa483614649fb0

--- src/perfetto_cmd/perfetto_cmd.cc
+++ src/perfetto_cmd/perfetto_cmd.cc
@@ -58,6 +58,7 @@ namespace perfetto {
 namespace {
 
 constexpr char kDefaultDropBoxTag[] = "perfetto";
+constexpr uint32_t kFlushTimeout = 1000;  // 1 s
 
 perfetto::PerfettoCmd* g_consumer_cmd;
 
@@ -503,8 +504,10 @@ void PerfettoCmd::SetupCtrlCSignalHandler() {
 #pragma GCC diagnostic pop
   sigaction(SIGINT, &sa, nullptr);
 
-  task_runner_.AddFileDescriptorWatch(
-      *ctrl_c_pipe_.rd, [this] { consumer_endpoint_->DisableTracing(); });
+  task_runner_.AddFileDescriptorWatch(*ctrl_c_pipe_.rd, [this] {
+    consumer_endpoint_->Flush(
+        kFlushTimeout, [this](bool) { consumer_endpoint_->DisableTracing(); });
+  });
 }
 
 int __attribute__((visibility("default")))
