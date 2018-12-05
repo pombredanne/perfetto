8c99bb0f08db20cf15af1cf7ba2b5963cc258fe9
Hector Dearman <hjd@google.com>
2018-12-05 16:28:19 +0000

    [Patchset 2] trace_processor: Add regression test for span join two views
    Bug: 120487929
    Change-Id: Ic719362b653aea66b5ca538a23e059fae67148e7

--- test/trace_processor/b120487929.sql
+++ test/trace_processor/b120487929.sql
@@ -3,11 +3,10 @@ create view freq_view as
     ts,
     dur,
     ref as cpu,
-    name,
-    value
+    name as freq_name,
+    value as freq_value
   from counters
   where name = 'cpufreq'
-    and ref = 0
     and ref_type = 'cpu';
 
 create view idle_view
@@ -15,14 +14,41 @@ create view idle_view
     ts,
     dur,
     ref as cpu,
-    name,
-    value
+    name as idle_name,
+    value as idle_value
   from counters
   where name = 'cpuidle'
-    and ref = 0
     and ref_type = 'cpu';
 
 create virtual table freq_idle
-              using span_join(freq_view PARTITIONED cpu,
-                              idle_view PARTITIONED cpu)
+  using span_join(freq_view PARTITIONED cpu, idle_view PARTITIONED cpu)
 
+create virtual table window_freq_idle using window;
+
+create virtual table span_freq_idle
+  using span_join(freq_idle PARTITIONED cpu, window_freq_idle PARTITIONED cpu)
+
+update window_freq_idle
+  set
+    window_start=(select min(ts) from sched),
+    window_dur=(select max(ts) - min(ts) from sched),
+    quantum=1000000
+  where rowid = 0
+
+create view counter_view
+  as select
+    ts,
+    dur,
+    quantum_ts,
+    cpu,
+    case idle_value
+      when 4294967295 then "freq"
+      else "idle"
+    end as name,
+    case idle_value
+      when 4294967295 then freq_value
+      else idle_value
+    end as value
+  from span_freq_idle
+
+select cpu, name, value, sum(dur) from counter_view group by cpu, name, value
