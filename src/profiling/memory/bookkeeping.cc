7768478d7cf1fb6572981dad3ebe5f1aedabdd10
Florian Mayer <fmayer@google.com>
2018-12-13 16:56:01 +0000

    [Patchset 4] profiling: Actually flush TraceWriter.
    Change-Id: I3c427be032efcec9d4f2d2ab4c5100179b4b5a39

--- src/profiling/memory/bookkeeping.cc
+++ src/profiling/memory/bookkeeping.cc
@@ -330,7 +330,7 @@ void BookkeepingThread::HandleBookkeepingRecord(BookkeepingRecord* rec) {
         it = bookkeeping_data_.erase(it);
       }
     }
-    profile_packet->Finalize();
+    trace_writer->Finalize();
     dump_rec.callback();
   } else if (rec->record_type == BookkeepingRecord::Type::Free) {
     FreeRecord& free_rec = rec->free_record;
