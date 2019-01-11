8e953c81cf525cde6fcb20f7f9a4ca7d9732973d
Florian Mayer <fmayer@google.com>
2019-01-11 18:30:28 +0000

    Add SharedRingBuffer
    Change-Id: Ief02a8f8a10d5d0a81cca03ce2864fb4cc744ba8

---
+++ src/profiling/memory/shared_ring_buffer.h
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SRC_PROFILING_MEMORY_SHARED_RING_BUFFER_H_
+#define SRC_PROFILING_MEMORY_SHARED_RING_BUFFER_H_
+
+#include "perfetto/base/optional.h"
+#include "perfetto/base/unix_socket.h"
+#include "perfetto/base/utils.h"
+
+#include <atomic>
+#include <map>
+#include <memory>
+
+#include <stdint.h>
+
+namespace perfetto {
+namespace profiling {
+
+class ScopedSpinlock {
+ public:
+  friend void swap(ScopedSpinlock&, ScopedSpinlock&);
+
+  enum class Mode { Try, Blocking };
+
+  ScopedSpinlock(std::atomic<bool>* lock, Mode mode);
+  ScopedSpinlock(const ScopedSpinlock&) = delete;
+  ScopedSpinlock& operator=(const ScopedSpinlock&) = delete;
+
+  ScopedSpinlock(ScopedSpinlock&&);
+  ScopedSpinlock& operator=(ScopedSpinlock&&);
+
+  ~ScopedSpinlock();
+
+  void Unlock() {
+    if (locked_) {
+      PERFETTO_DCHECK(lock_->load());
+      lock_->store(false, std::memory_order_release);
+    }
+    locked_ = false;
+  }
+
+  bool locked() const { return locked_; }
+
+ private:
+  std::atomic<bool>* lock_;
+  bool locked_ = false;
+};
+
+void swap(ScopedSpinlock&, ScopedSpinlock&);
+
+// A concurrent, multi-writer single-reader ring buffer FIFO, based on a
+// circular buffer over shared memory. It has similar semantics to a SEQ_PACKET
+// + O_NONBLOCK socket, specifically:
+//
+// - Writes are atomic, data is either written fully in the buffer or not.
+// - New writes are discarded if the buffer is full.
+// - If a write succeeds, the reader is guaranteed to see the whole buffer.
+// - Reads are atomic, no fragmentation.
+// - The reader sees writes in write order (% discarding).
+//
+// This class assumes that reader and write trust each other. Don't use in
+// untrusted contexts.
+//
+// TODO:
+// - Write a benchmark.
+// - Make the stats ifdef-able.
+class SharedRingBuffer {
+ public:
+  class WriteBuffer {
+   public:
+    friend class SharedRingBuffer;
+    uint8_t* buf();
+    size_t size() { return size_; }
+
+    operator bool() { return wr_ptr_ != nullptr; }
+
+   private:
+    size_t size_ = 0;
+    uint8_t* wr_ptr_ = nullptr;
+    uint64_t write_pos_ = 0;
+    size_t size_with_header_ = 0;
+  };
+
+  class ReadBuffer {
+   public:
+    friend class SharedRingBuffer;
+    friend void swap(ReadBuffer&, ReadBuffer&);
+
+    ReadBuffer() = default;
+    ~ReadBuffer();
+
+    ReadBuffer(const ReadBuffer&) = delete;
+    ReadBuffer& operator=(const ReadBuffer&) = delete;
+
+    ReadBuffer(ReadBuffer&&) noexcept;
+    ReadBuffer& operator=(ReadBuffer&&) noexcept;
+
+    uint8_t* data() const { return data_; }
+    size_t size() const { return size_; }
+    operator bool() const { return ring_buffer_ != nullptr; }
+
+   private:
+    ReadBuffer(uint8_t* data,
+               size_t size,
+               size_t size_with_header,
+               SharedRingBuffer* ring_buffer)
+        : data_(data),
+          size_(size),
+          size_with_header_(size_with_header),
+          ring_buffer_(ring_buffer) {}
+
+    uint8_t* data_ = nullptr;
+    size_t size_ = 0;
+    size_t size_with_header_ = 0;
+    SharedRingBuffer* ring_buffer_ = nullptr;
+  };
+
+  static base::Optional<SharedRingBuffer> Create(size_t);
+  static base::Optional<SharedRingBuffer> Attach(base::ScopedFile);
+
+  ~SharedRingBuffer();
+  SharedRingBuffer(SharedRingBuffer&&) noexcept;
+  SharedRingBuffer& operator=(SharedRingBuffer&&);
+
+  bool is_valid() const { return !!mem_; }
+  size_t size() const { return size_; }
+  int fd() const { return *mem_fd_; }
+
+  WriteBuffer BeginWrite(const ScopedSpinlock& spinlock, size_t size);
+  void EndWrite(const WriteBuffer& buf);
+
+  ReadBuffer Read();
+  ScopedSpinlock AcquireLock(ScopedSpinlock::Mode mode) {
+    return ScopedSpinlock(&meta_->spinlock, mode);
+  }
+
+ private:
+  struct alignas(base::kPageSize) MetadataPage {
+    std::atomic<bool> spinlock;
+    uint64_t read_pos;
+    uint64_t write_pos;
+
+    // stats, for debugging only.
+    std::atomic<uint64_t> failed_spinlocks;
+    std::atomic<uint64_t> bytes_written;
+    std::atomic<uint64_t> num_writes_succeeded;
+    std::atomic<uint64_t> num_writes_failed;
+    std::atomic<uint64_t> num_reads_failed;
+
+    // TODO static assert offsets.
+  };
+
+  struct CreateFlag {};
+  struct AttachFlag {};
+  SharedRingBuffer(const SharedRingBuffer&) = delete;
+  SharedRingBuffer& operator=(const SharedRingBuffer&) = delete;
+  SharedRingBuffer(CreateFlag, size_t size);
+  SharedRingBuffer(AttachFlag, base::ScopedFile mem_fd) {
+    Initialize(std::move(mem_fd));
+  }
+
+  void Initialize(base::ScopedFile mem_fd);
+  bool IsCorrupt();
+  void EndRead(const ReadBuffer&);
+
+  inline size_t read_avail(const ScopedSpinlock&) {
+    PERFETTO_DCHECK(meta_->write_pos >= meta_->read_pos);
+    auto res = static_cast<size_t>(meta_->write_pos - meta_->read_pos);
+    PERFETTO_DCHECK(res <= size_);
+    return res;
+  }
+
+  inline size_t write_avail(const ScopedSpinlock& spinlock) {
+    return size_ - read_avail(spinlock);
+  }
+
+  inline uint8_t* at(uint64_t pos) { return mem_ + (pos & (size_ - 1)); }
+
+  base::ScopedFile mem_fd_;
+  MetadataPage* meta_ = nullptr;  // Start of the mmaped region.
+  uint8_t* mem_ = nullptr;  // Start of the contents (i.e. meta_ + kPageSize).
+
+  // Size of the ring buffer contents, without including metadata or the 2nd
+  // mmap.
+  size_t size_ = 0;
+
+  // Remember to update the move ctor when adding new fields.
+};
+
+void swap(SharedRingBuffer::ReadBuffer&, SharedRingBuffer::ReadBuffer&);
+
+}  // namespace profiling
+}  // namespace perfetto
+
+#endif  // SRC_PROFILING_MEMORY_SHARED_RING_BUFFER_H_
