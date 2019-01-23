16bb1268b986f9dcc431bfb4cd48eabad6cde887
Lalit Maganti <lalitm@google.com>
2019-01-23 18:31:14 +0000

    [Patchset 9] trace_processor: add support for polled oom_score_adj
    Bug: 122885617
    Change-Id: I325c8245734ed7e0edf7cccbe232dcbaa89236c3

--- src/trace_processor/proto_trace_parser.h
+++ src/trace_processor/proto_trace_parser.h
@@ -84,6 +84,7 @@ class ProtoTraceParser {
   void ParseLowmemoryKill(int64_t ts, TraceBlobView);
   void ParseBatteryCounters(int64_t ts, TraceBlobView);
   void ParseOOMScoreAdjUpdate(int64_t ts, TraceBlobView);
+  void ParseMmEventRecordField(int64_t ts, uint32_t pid, TraceBlobView);
   void ParseClockSnapshot(TraceBlobView);
   std::pair<int /*type*/, int64_t> ParseClockField(TraceBlobView);
   void ParseAndroidLogPacket(TraceBlobView);
@@ -140,6 +141,20 @@ class ProtoTraceParser {
   // ProcessStats::process.
   static constexpr size_t kProcStatsProcessSize = 11;
   std::array<StringId, kProcStatsProcessSize> proc_stats_process_names_{};
+
+  struct MmEventCounterNames {
+    MmEventCounterNames() = default;
+    MmEventCounterNames(StringId _count, StringId _max_lat, StringId _avg_lat)
+        : count(_count), max_lat(_max_lat), avg_lat(_avg_lat) {}
+
+    StringId count = 0;
+    StringId max_lat = 0;
+    StringId avg_lat = 0;
+  };
+
+  // Keep kMmEventCounterSize equal to mm_event_type::MM_TYPE_NUM in the kernel.
+  static constexpr size_t kMmEventCounterSize = 7;
+  std::array<MmEventCounterNames, kMmEventCounterSize> mm_event_counter_names_;
 };
 
 }  // namespace trace_processor
