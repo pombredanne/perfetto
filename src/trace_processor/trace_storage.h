845fc8051767e25bbaa27c4ab921ec4610592147
Stephen Nusko <nuskos@google.com>
2019-01-24 14:48:03 +0000

    [Patchset 6] Update perfetto task runner to know if it will run the task on the same thread or not.
    This is needed for https://bugs.chromium.org/p/chromium/issues/detail?id=839074
    so that we can efficently choose to flush the current chunk or post the
    task to the task runner if we're in a different thread.
    
    Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=839074
    Change-Id: I990d19c832af6573db2de35049ff88584c94cad8

--- src/trace_processor/trace_storage.h
+++ src/trace_processor/trace_storage.h
@@ -146,6 +146,13 @@ class TraceStorage {
     size_t args_count() const { return ids_.size(); }
 
     void AddArg(RowId id, StringId flat_key, StringId key, Variadic value) {
+      // TODO(b/123252504): disable this code to stop blow-ups in ingestion time
+      // and memory.
+      perfetto::base::ignore_result(id);
+      perfetto::base::ignore_result(flat_key);
+      perfetto::base::ignore_result(key);
+      perfetto::base::ignore_result(value);
+      /*
       if (id == kInvalidRowId)
         return;
 
@@ -154,6 +161,7 @@ class TraceStorage {
       keys_.emplace_back(key);
       arg_values_.emplace_back(value);
       args_for_id_.emplace(id, static_cast<uint32_t>(args_count() - 1));
+      */
     }
 
    private:
@@ -174,6 +182,7 @@ class TraceStorage {
       start_ns_.emplace_back(start_ns);
       durations_.emplace_back(duration_ns);
       utids_.emplace_back(utid);
+      rows_for_utids_.emplace(utid, slice_count() - 1);
       return slice_count() - 1;
     }
 
@@ -191,6 +200,10 @@ class TraceStorage {
 
     const std::deque<UniqueTid>& utids() const { return utids_; }
 
+    const std::multimap<UniqueTid, uint32_t>& rows_for_utids() const {
+      return rows_for_utids_;
+    }
+
    private:
     // Each deque below has the same number of entries (the number of slices
     // in the trace for the CPU).
@@ -198,6 +211,7 @@ class TraceStorage {
     std::deque<int64_t> start_ns_;
     std::deque<int64_t> durations_;
     std::deque<UniqueTid> utids_;
+    std::multimap<UniqueTid, uint32_t> rows_for_utids_;
   };
 
   class NestableSlices {
@@ -520,11 +534,11 @@ class TraceStorage {
 
   // |unique_processes_| always contains at least 1 element becuase the 0th ID
   // is reserved to indicate an invalid process.
-  size_t process_count() const { return unique_processes_.size() - 1; }
+  size_t process_count() const { return unique_processes_.size(); }
 
   // |unique_threads_| always contains at least 1 element becuase the 0th ID
   // is reserved to indicate an invalid thread.
-  size_t thread_count() const { return unique_threads_.size() - 1; }
+  size_t thread_count() const { return unique_threads_.size(); }
 
   // Number of interned strings in the pool. Includes the empty string w/ ID=0.
   size_t string_count() const { return string_pool_.size(); }
