101615c6fd1bb7afb090247720388df642259642
Florian Mayer <fmayer@google.com>
2019-01-28 16:42:58 +0000

    [Patchset 26] Add SharedRingBuffer
    Change-Id: Ief02a8f8a10d5d0a81cca03ce2864fb4cc744ba8

--- src/trace_processor/proto_trace_parser.cc
+++ src/trace_processor/proto_trace_parser.cc
@@ -21,6 +21,7 @@
 #include <string>
 
 #include "perfetto/base/logging.h"
+#include "perfetto/base/optional.h"
 #include "perfetto/base/string_view.h"
 #include "perfetto/base/utils.h"
 #include "perfetto/protozero/proto_decoder.h"
@@ -965,13 +966,15 @@ void ProtoTraceParser::ParsePrint(uint32_t,
           context_->storage->mutable_instants()->AddInstantEvent(
               timestamp, lmk_id_, 0, killed_upid, RefType::kRefUpid);
         }
-        // TODO(tilal6991): we should not add LMK events to the counters table
+        // TODO(lalitm): we should not add LMK events to the counters table
         // once the UI has support for displaying instants.
       }
-      UniqueTid utid = context_->process_tracker->UpdateThread(pid, point.tgid);
+      // This is per upid on purpose. Some counters are pushed from arbitrary
+      // threads but are really per process.
+      UniquePid upid = context_->process_tracker->UpdateProcess(point.tgid);
       StringId name_id = context_->storage->InternString(point.name);
       context_->event_tracker->PushCounter(timestamp, point.value, name_id,
-                                           utid, RefType::kRefUtid);
+                                           upid, RefType::kRefUpid);
     }
   }
   PERFETTO_DCHECK(decoder.IsEndOfBuffer());
@@ -1349,13 +1352,15 @@ void ProtoTraceParser::ParseAndroidLogEvent(TraceBlobView event) {
     msg_id = context_->storage->InternString(&arg_msg[1]);
   }
   UniquePid utid = tid ? context_->process_tracker->UpdateThread(tid, pid) : 0;
-  int64_t trace_time =
+  base::Optional<int64_t> opt_trace_time =
       context_->clock_tracker->ToTraceTime(ClockDomain::kRealTime, ts);
+  if (!opt_trace_time)
+    return;
 
   // Log events are NOT required to be sorted by trace_time. The virtual table
   // will take care of sorting on-demand.
-  context_->storage->mutable_android_log()->AddLogEvent(trace_time, utid, prio,
-                                                        tag_id, msg_id);
+  context_->storage->mutable_android_log()->AddLogEvent(
+      opt_trace_time.value(), utid, prio, tag_id, msg_id);
 }
 
 void ProtoTraceParser::ParseAndroidLogBinaryArg(TraceBlobView arg,
