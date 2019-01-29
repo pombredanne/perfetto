6100b3aca7a3c008e099ec2b3d292540314abcd8
Florian Mayer <fmayer@google.com>
2019-01-29 11:15:58 +0000

    [Patchset 6] profiling: Improve e2e test.
    Change-Id: Ie44c4df0398f914ed59cc52079c1b59113cb6632

--- src/profiling/memory/unwinding.cc
+++ src/profiling/memory/unwinding.cc
@@ -171,10 +171,14 @@ bool DoUnwind(WireMessage* msg, UnwindingMetadata* metadata, AllocRecord* out) {
   std::unique_ptr<unwindstack::Regs> regs(
       CreateFromRawData(alloc_metadata->arch, alloc_metadata->register_data));
   if (regs == nullptr) {
+    unwindstack::FrameData frame_data{};
+    frame_data.function_name = "ERROR READING REGISTERS";
+    frame_data.map_name = "ERROR";
+
+    out->frames.emplace_back(frame_data, "");
     PERFETTO_DLOG("regs");
     return false;
   }
-  out->alloc_metadata = *alloc_metadata;
   uint8_t* stack = reinterpret_cast<uint8_t*>(msg->payload);
   std::shared_ptr<unwindstack::Memory> mems =
       std::make_shared<StackOverlayMemory>(metadata->fd_mem,
@@ -202,20 +206,28 @@ bool DoUnwind(WireMessage* msg, UnwindingMetadata* metadata, AllocRecord* out) {
     if (error_code != unwindstack::ERROR_INVALID_MAP)
       break;
   }
-  out->frames = unwinder.frames();
+  for (unwindstack::FrameData fd : unwinder.frames()) {
+    std::string build_id;
+    if (fd.map_name != "") {
+      unwindstack::MapInfo* map_info = metadata->maps.Find(fd.pc);
+      if (map_info)
+        build_id = map_info->GetBuildID();
+    }
+
+    if (base::EndsWith(fd.map_name, ".so"))
+      MaybeDemangle(&fd.function_name);
+    out->frames.emplace_back(std::move(fd), std::move(build_id));
+  }
+
   if (error_code != 0) {
     unwindstack::FrameData frame_data{};
     frame_data.function_name = "ERROR " + std::to_string(error_code);
     frame_data.map_name = "ERROR";
 
-    out->frames.emplace_back(frame_data);
+    out->frames.emplace_back(frame_data, "");
     PERFETTO_DLOG("unwinding failed %" PRIu8, error_code);
   }
 
-  for (unwindstack::FrameData& fd : out->frames) {
-    if (base::EndsWith(fd.map_name, ".so"))
-      MaybeDemangle(&fd.function_name);
-  }
   return true;
 }
 
@@ -231,9 +243,11 @@ bool HandleUnwindingRecord(UnwindingRecord* rec, BookkeepingRecord* out) {
       return false;
     }
 
+    out->alloc_record.alloc_metadata = *msg.alloc_header;
     out->pid = rec->pid;
     out->record_type = BookkeepingRecord::Type::Malloc;
-    return DoUnwind(&msg, metadata.get(), &out->alloc_record);
+    DoUnwind(&msg, metadata.get(), &out->alloc_record);
+    return true;
   } else if (msg.record_type == RecordType::Free) {
     out->record_type = BookkeepingRecord::Type::Free;
     out->pid = rec->pid;
--- ui/src/frontend/track_panel.ts
+++ ui/src/frontend/track_panel.ts
@@ -21,11 +21,8 @@ import {globals} from './globals';
 import {drawGridLines} from './gridline_helper';
 import {Panel, PanelSize} from './panel';
 import {Track} from './track';
+import {TRACK_SHELL_WIDTH} from './track_constants';
 import {trackRegistry} from './track_registry';
-
-// TODO(hjd): We should remove the constant where possible.
-// If any uses can't be removed we should read this constant from CSS.
-export const TRACK_SHELL_WIDTH = 250;
 
 function isPinned(id: string) {
   return globals.state.pinnedTracks.indexOf(id) !== -1;
@@ -58,7 +55,12 @@ class TrackShell implements m.ClassComponent<TrackShellAttrs> {
           ondragleave: this.ondragleave.bind(this),
           ondrop: this.ondrop.bind(this),
         },
-        m('h1', attrs.trackState.name),
+        m('h1',
+          {
+            title: attrs.trackState.name,
+          },
+          attrs.trackState.name,
+          m.trust('&#x200E;')),
         m(TrackButton, {
           action: Actions.toggleTrackPinned({trackId: attrs.trackState.id}),
           i: isPinned(attrs.trackState.id) ? 'star' : 'star_border',
