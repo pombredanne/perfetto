31cf08ac53b7fc4b73260d0f215f7eefd8e8daf7
Isabelle Taylor <taylori@google.com>
2019-01-31 10:58:37 +0000

    [Patchset 13] perfetto-ui: Show slice details on click
    You can now click on a slice and see the details
    about it in the panel below.
    
    This is just the logic to display the info, improvements to be made
    in future CLs include:
    - formatting the info nicely
    - unselecting a slice
    - showing which slice has been selected in some way
    - showing the panel on click
    
    Bug:118893776
    Change-Id: Id796149ae5a02105c2eeb436544c2c944df8846b

--- src/trace_processor/trace_storage.h
+++ src/trace_processor/trace_storage.h
@@ -189,7 +189,10 @@ class TraceStorage {
       utids_.emplace_back(utid);
       end_states_.emplace_back(end_state);
       priorities_.emplace_back(priority);
-      rows_for_utids_.emplace(utid, slice_count() - 1);
+
+      if (utid >= rows_for_utids_.size())
+        rows_for_utids_.resize(utid + 1);
+      rows_for_utids_[utid].emplace_back(slice_count() - 1);
       return slice_count() - 1;
     }
 
@@ -217,7 +220,7 @@ class TraceStorage {
 
     const std::deque<int32_t>& priorities() const { return priorities_; }
 
-    const std::multimap<UniqueTid, uint32_t>& rows_for_utids() const {
+    const std::deque<std::vector<uint32_t>>& rows_for_utids() const {
       return rows_for_utids_;
     }
 
@@ -230,7 +233,9 @@ class TraceStorage {
     std::deque<UniqueTid> utids_;
     std::deque<ftrace_utils::TaskState> end_states_;
     std::deque<int32_t> priorities_;
-    std::multimap<UniqueTid, uint32_t> rows_for_utids_;
+
+    // One row per utid.
+    std::deque<std::vector<uint32_t>> rows_for_utids_;
   };
 
   class NestableSlices {
