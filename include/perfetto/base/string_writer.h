5043433559598e9b7ee6b32177c15a7b3d1321ce
Lalit Maganti <lalitm@google.com>
2019-02-06 15:18:24 +0000

    [Patchset 1] perfetto: add string writer class for formatting strings
    This class acts as a replacement for snprintf for most circumstances and
    is far faster in performance sensitive contexts.
    
    Change-Id: Idf0d81dd8446f9a8858b9aee15be6931d298454b

---
+++ include/perfetto/base/string_writer.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INCLUDE_PERFETTO_BASE_STRING_WRITER_H_
+#define INCLUDE_PERFETTO_BASE_STRING_WRITER_H_
+
+#include "perfetto/base/logging.h"
+#include "perfetto/base/string_view.h"
+
+namespace perfetto {
+namespace base {
+
+// A helper class which writes formatted data to a string buffer.
+class StringWriter {
+ public:
+  // Creates a string buffer from a char buffer and length.
+  StringWriter(char* buffer, size_t n) : buffer_(buffer), n_(n) {}
+
+  // Writes a char to the buffer.
+  void WriteChar(const char in) {
+    PERFETTO_DCHECK(pos_ + 1 <= n_);
+    buffer_[pos_++] = in;
+  }
+
+  // Writes a length delimited string to the buffer.
+  void WriteString(const char* in, size_t n) {
+    PERFETTO_DCHECK(pos_ + n <= n_);
+    memcpy(&buffer_[pos_], in, n);
+    pos_ += n;
+  }
+
+  // Writes a StringView to the buffer.
+  void WriteString(StringView data) {
+    PERFETTO_DCHECK(pos_ + data.size() <= n_);
+    memcpy(&buffer_[pos_], data.data(), data.size());
+    pos_ += data.size();
+  }
+
+  // Writes an integer to the buffer.
+  void WriteInt(int64_t value) { WritePaddedInt<'0', 0>(value); }
+
+  // Writes an integer to the buffer, padding with |PadChar| if the number of
+  // digits of the integer is less than Padding.
+  template <char PadChar, size_t Padding>
+  void WritePaddedInt(int64_t value) {
+    // Need to add 2 to the number of digits to account for minus sign and
+    // rounding down of digits10.
+    constexpr auto kBufferSize =
+        std::numeric_limits<uint64_t>::digits10 + 2 + Padding;
+    PERFETTO_DCHECK(pos_ + kBufferSize <= n_);
+
+    char data[kBufferSize];
+    bool negate = value < 0;
+    if (negate)
+      value = 0 - value;
+
+    uint64_t val = static_cast<uint64_t>(value);
+    size_t idx;
+    for (idx = kBufferSize - 1; val >= 10;) {
+      char digit = val % 10;
+      val /= 10;
+      data[idx--] = digit + '0';
+    }
+    data[idx--] = static_cast<char>(val) + '0';
+
+    if (Padding > 0) {
+      size_t num_digits = kBufferSize - 1 - idx;
+      for (size_t i = num_digits; i < Padding; i++) {
+        data[idx--] = PadChar;
+      }
+    }
+
+    if (negate)
+      buffer_[pos_++] = '-';
+    for (size_t i = idx + 1; i < kBufferSize; i++)
+      buffer_[pos_++] = data[i];
+  }
+
+  char* GetCString() {
+    PERFETTO_DCHECK(pos_ < n_);
+    buffer_[pos_] = '\0';
+    return buffer_;
+  }
+
+ private:
+  char* buffer_ = nullptr;
+  size_t n_ = 0;
+  size_t pos_ = 0;
+};
+
+}  // namespace base
+}  // namespace perfetto
+
+#endif  // INCLUDE_PERFETTO_BASE_STRING_WRITER_H_
---
+++ src/base/string_writer_unittest.cc
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "perfetto/base/string_writer.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace perfetto {
+namespace base {
+namespace {
+
+TEST(StringWriterTest, BasicCases) {
+  char buffer[128];
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WriteChar('0');
+    ASSERT_STREQ(writer.GetCString(), "0");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WriteInt(132545);
+    ASSERT_STREQ(writer.GetCString(), "132545");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WritePaddedInt<'0', 3>(0);
+    ASSERT_STREQ(writer.GetCString(), "000");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WritePaddedInt<'0', 1>(1);
+    ASSERT_STREQ(writer.GetCString(), "1");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WritePaddedInt<'0', 3>(1);
+    ASSERT_STREQ(writer.GetCString(), "001");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WritePaddedInt<'0', 0>(1);
+    ASSERT_STREQ(writer.GetCString(), "1");
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WritePaddedInt<' ', 5>(123);
+    ASSERT_STREQ(writer.GetCString(), "  123");
+  }
+
+  constexpr char kTestStr[] = "test";
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WriteString(kTestStr, sizeof(kTestStr) - 1);
+    ASSERT_STREQ(writer.GetCString(), kTestStr);
+  }
+  {
+    base::StringWriter writer(buffer, sizeof(buffer));
+    writer.WriteString(kTestStr);
+    ASSERT_STREQ(writer.GetCString(), kTestStr);
+  }
+}
+
+TEST(StringWriterTest, WriteAllTypes) {
+  char buffer[128];
+  base::StringWriter writer(buffer, sizeof(buffer));
+  writer.WriteChar('0');
+  writer.WriteInt(132545);
+  writer.WritePaddedInt<'0', 0>(1);
+  writer.WritePaddedInt<'0', 3>(0);
+  writer.WritePaddedInt<'0', 1>(1);
+  writer.WritePaddedInt<'0', 2>(1);
+  writer.WritePaddedInt<'0', 3>(1);
+  writer.WritePaddedInt<' ', 5>(123);
+
+  constexpr char kTestStr[] = "test";
+  writer.WriteString(kTestStr, sizeof(kTestStr) - 1);
+  writer.WriteString(kTestStr);
+
+  ASSERT_STREQ(writer.GetCString(), "01325451000101001  123testtest");
+}
+
+}  // namespace
+}  // namespace base
+}  // namespace perfetto
