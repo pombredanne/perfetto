2e53cbe8e1d2978ea2049231ab53bd2150efd49a
Lalit Maganti <lalitm@google.com>
2019-02-07 16:35:37 +0000

    [Patchset 3] trace_processor: improve trace processor testing infra
    Change-Id: Id574c520a740b0eb76fa7516d0dcc5596539c751

--- tools/add_tp_diff_test.sh
+++ tools/add_tp_diff_test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-read -p "Name of SQL file to create (in test/trace_processor): " sqlfile
-read -p "Path to trace file: " tracefile
+set -e
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-ROOTDIR=$(dirname $ABSDIR)
-TRACE_PROC_PATH=$ROOTDIR/test/trace_processor
+read -p "Name of SQL file to create (in test/trace_processor): " sqlfile
+read -p "Name to trace file (in test/): " tracefile
+
+ROOTDIR=$(dirname $(dirname $(readlink -f $0)))
+TEST_PATH=$ROOTDIR/test
+TRACE_PROC_PATH=$TEST_PATH/trace_processor
 
 SQL_FILE_NAME=${sqlfile%.*}
 
 echo "Creating $TRACE_PROC_PATH/$sqlfile"
 touch $TRACE_PROC_PATH/$sqlfile
 
-RELTRACE=$(realpath -s $tracefile --relative-to=$TRACE_PROC_PATH --relative-base=$ROOTDIR)
-TRACE_BASE=$(basename $RELTRACE)
+TRACE_PATH=$TEST_PATH/$tracefile
+TRACE_BASE=$(basename $tracefile)
 TRACE_FILE_NAME=${TRACE_BASE%.*}
 OUT_FILE="$SQL_FILE_NAME""_$TRACE_FILE_NAME.out"
 
 echo "Creating $TRACE_PROC_PATH/$OUT_FILE"
 touch $TRACE_PROC_PATH/$OUT_FILE
 
-echo "Now add the following line to $ROOTDIR/test/tace_processor/index"
-echo "$RELTRACE $sqlfile $OUT_FILE"
+RELTRACE=$(realpath -s $TRACE_PATH --relative-to=$TRACE_PROC_PATH --relative-base=$ROOTDIR)
+echo "Adding index line to $TRACE_PROC_PATH/index"
+echo >> $TRACE_PROC_PATH/index
+echo "$RELTRACE $sqlfile $OUT_FILE" >> $TRACE_PROC_PATH/index
