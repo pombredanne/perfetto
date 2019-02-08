9b6978d7076eb6bc8db9b9a66cd00ad314e5ab2e
Lalit Maganti <lalitm@google.com>
2019-02-08 11:28:09 +0000

    [Patchset 13] trace_processor: optimise adding sched raw events
    As sched events are so frequent and interning strings and finding utids
    is relatively expensive, we use our specialised knowledge about sched_switch
    to avoid intern and utid lookup operations.
    
    Bug: 122513680
    Change-Id: I2405fa0c4f25ad753ff6a76e0c4c79cd62e8dbe6

--- src/profiling/memory/shared_ring_buffer_fuzzer.cc
+++ src/profiling/memory/shared_ring_buffer_fuzzer.cc
@@ -53,14 +53,14 @@ int FuzzRingBuffer(const uint8_t* data, size_t size) {
   PERFETTO_CHECK(base::WriteAll(*fd, data, sizeof(MetadataHeader)) != -1);
   PERFETTO_CHECK(lseek(*fd, base::kPageSize, SEEK_SET) != -1);
 
+  // Put the remaining fuzzer input into the data portion of the ring buffer.
   size_t payload_size = size - sizeof(MetadataHeader);
   const uint8_t* payload = data + sizeof(MetadataHeader);
   size_t payload_size_pages =
       (payload_size + base::kPageSize - 1) / base::kPageSize;
-  // The size of the memory pointed to by the file-descriptor (including the
-  // metdata page) needs to be 2^n pages.
-  size_t total_size_pages = RoundToPow2(1 + payload_size_pages);
-
+  // Upsize test buffer to be 2^n data pages (precondition of the impl) + 1 page
+  // for the metadata.
+  size_t total_size_pages = 1 + RoundToPow2(payload_size_pages);
   PERFETTO_CHECK(base::WriteAll(*fd, payload, payload_size) != -1);
   if (base::kPageSize + payload_size != total_size_pages * base::kPageSize) {
     PERFETTO_CHECK(
@@ -72,17 +72,18 @@ int FuzzRingBuffer(const uint8_t* data, size_t size) {
 
   auto buf = SharedRingBuffer::Attach(std::move(fd));
   PERFETTO_CHECK(!!buf);
-  SharedRingBuffer::Buffer read_buf = {};
+  bool did_read;
   do {
-    read_buf = buf->BeginRead();
-    if (read_buf) {
+    auto read_buf = buf->BeginRead();
+    did_read = bool(read_buf);
+    if (did_read) {
       volatile uint8_t* v_data = read_buf.data;
       // Assert we get a reference to valid memory.
       for (size_t i = 0; i < read_buf.size; ++i)
         v_data[i] = v_data[i];
     }
     buf->EndRead(std::move(read_buf));
-  } while (read_buf);
+  } while (did_read);
   return 0;
 }
 
