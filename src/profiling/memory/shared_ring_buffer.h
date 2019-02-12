85ae6c8c11cb9457479ea75ad6c117fd23ef9c6a
Florian Mayer <fmayer@google.com>
2019-02-12 11:06:25 +0000

    [Patchset 1] Protect SharedRingbuffer against concurrent manipulation.
    Change-Id: I5b55b82352e7f999acc0445d215a461f2c3c0454

--- src/profiling/memory/shared_ring_buffer.h
+++ src/profiling/memory/shared_ring_buffer.h
@@ -76,9 +76,6 @@ class ScopedSpinlock {
 // - Reads are atomic, no fragmentation.
 // - The reader sees writes in write order (% discarding).
 //
-// This class assumes that reader and write trust each other. Don't use in
-// untrusted contexts.
-//
 // TODO:
 // - Write a benchmark.
 // - Make the stats ifdef-able.
@@ -139,6 +136,11 @@ class SharedRingBuffer {
     std::atomic<uint64_t> num_reads_failed;
   };
 
+  struct PointerPositions {
+    uint64_t read_pos;
+    uint64_t write_pos;
+  };
+
   struct CreateFlag {};
   struct AttachFlag {};
   SharedRingBuffer(const SharedRingBuffer&) = delete;
@@ -149,21 +151,29 @@ class SharedRingBuffer {
   }
 
   void Initialize(base::ScopedFile mem_fd);
-  bool IsCorrupt();
+  bool IsCorrupt(const PointerPositions& pos);
 
-  inline size_t read_avail(const ScopedSpinlock& lock) {
+  inline PointerPositions GetPointerPositions(const ScopedSpinlock& lock) {
     PERFETTO_DCHECK(lock.locked());
-    PERFETTO_DCHECK(meta_->write_pos >= meta_->read_pos);
-    if (meta_->read_pos > meta_->write_pos)
+
+    PointerPositions pos;
+    pos.read_pos = meta_->read_pos;
+    pos.write_pos = meta_->write_pos;
+    return pos;
+  }
+
+  inline size_t read_avail(const PointerPositions& pos) {
+    PERFETTO_DCHECK(pos.write_pos >= pos.read_pos);
+    if (pos.read_pos > pos.write_pos)
       return 0;
 
-    auto res = static_cast<size_t>(meta_->write_pos - meta_->read_pos);
+    auto res = static_cast<size_t>(pos.write_pos - pos.read_pos);
     PERFETTO_DCHECK(res <= size_);
     return res;
   }
 
-  inline size_t write_avail(const ScopedSpinlock& spinlock) {
-    return size_ - read_avail(spinlock);
+  inline size_t write_avail(const PointerPositions& pos) {
+    return size_ - read_avail(pos);
   }
 
   inline uint8_t* at(uint64_t pos) { return mem_ + (pos & (size_ - 1)); }
