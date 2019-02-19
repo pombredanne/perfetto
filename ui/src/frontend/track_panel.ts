04816cedd44ace94e4b49a5a825662cc8004578c
Eric Seckler <eseckler@google.com>
2019-02-19 13:56:49 +0000

    [Patchset 15] protos: Add v0 of new TraceEvent protos
    Adds the new TraceEvent protos for events emitted by the TRACE_EVENT
    macro instrumentation in client applications. Also adds the necessary
    infrastructure for interned data to TracePacket.
    
    The TraceEvent proto will eventually replace the ChromeTraceEvent and
    ChromeEventBundle proto.
    
    Bug: 123864183
    Change-Id: I32963b8f53afcfe4892de919d8b27cd0a46a965a

--- ui/src/frontend/track_panel.ts
+++ ui/src/frontend/track_panel.ts
@@ -19,7 +19,7 @@ import {TrackState} from '../common/state';
 
 import {globals} from './globals';
 import {drawGridLines} from './gridline_helper';
-import {drawVerticalLine} from './vertical_line_helper';
+import {drawVerticalLine, drawVerticalSelection} from './vertical_line_helper';
 import {Panel, PanelSize} from './panel';
 import {Track} from './track';
 import {TRACK_SHELL_WIDTH} from './track_constants';
@@ -75,10 +75,12 @@ class TrackShell implements m.ClassComponent<TrackShellAttrs> {
   }
 
   ondragstart(e: DragEvent) {
+    const dataTransfer = e.dataTransfer;
+    if (dataTransfer === null) return;
     this.dragging = true;
     globals.rafScheduler.scheduleFullRedraw();
-    e.dataTransfer.setData('perfetto/track', `${this.attrs!.trackState.id}`);
-    e.dataTransfer.setDragImage(new Image(), 0, 0);
+    dataTransfer.setData('perfetto/track', `${this.attrs!.trackState.id}`);
+    dataTransfer.setDragImage(new Image(), 0, 0);
     e.stopImmediatePropagation();
   }
 
@@ -90,8 +92,10 @@ class TrackShell implements m.ClassComponent<TrackShellAttrs> {
   ondragover(e: DragEvent) {
     if (this.dragging) return;
     if (!(e.target instanceof HTMLElement)) return;
-    if (!e.dataTransfer.types.includes('perfetto/track')) return;
-    e.dataTransfer.dropEffect = 'move';
+    const dataTransfer = e.dataTransfer;
+    if (dataTransfer === null) return;
+    if (!dataTransfer.types.includes('perfetto/track')) return;
+    dataTransfer.dropEffect = 'move';
     e.preventDefault();
 
     // Apply some hysteresis to the drop logic so that the lightened border
@@ -111,8 +115,10 @@ class TrackShell implements m.ClassComponent<TrackShellAttrs> {
 
   ondrop(e: DragEvent) {
     if (this.dropping === undefined) return;
+    const dataTransfer = e.dataTransfer;
+    if (dataTransfer === null) return;
     globals.rafScheduler.scheduleFullRedraw();
-    const srcId = e.dataTransfer.getData('perfetto/track');
+    const srcId = dataTransfer.getData('perfetto/track');
     const dstId = this.attrs!.trackState.id;
     globals.dispatch(Actions.moveTrack({srcId, op: this.dropping, dstId}));
     this.dropping = undefined;
@@ -132,6 +138,10 @@ export class TrackContent implements m.ClassComponent<TrackContentAttrs> {
         globals.rafScheduler.scheduleRedraw();
       },
       onclick: (e:MouseEvent) => {
+        // If we are selecting a timespan - do not pass the click to the track.
+        const selection = globals.state.currentSelection;
+        if (selection && selection.kind === 'TIMESPAN') return;
+
         if (attrs.track.onMouseClick({x: e.layerX, y: e.layerY})) {
           e.stopPropagation();
         }
@@ -222,15 +232,23 @@ export class TrackPanel extends Panel<TrackPanelAttrs> {
                        `#aaa`);
     }
 
-    // Draw vertical line when a note is selected.
-    if (globals.state.currentSelection !== null &&
-        globals.state.currentSelection.kind === 'NOTE') {
-      const note = globals.state.notes[globals.state.currentSelection.id];
-      drawVerticalLine(ctx,
-                       localState.timeScale,
-                       note.timestamp,
-                       size.height,
-                       note.color);
+    if (globals.state.currentSelection !== null) {
+      if (globals.state.currentSelection.kind === 'NOTE') {
+        const note = globals.state.notes[globals.state.currentSelection.id];
+        drawVerticalLine(ctx,
+                        localState.timeScale,
+                        note.timestamp,
+                        size.height,
+                        note.color);
+      }
+      if (globals.state.currentSelection.kind === 'TIMESPAN') {
+        drawVerticalSelection(ctx,
+                              localState.timeScale,
+                              globals.state.currentSelection.startTs,
+                              globals.state.currentSelection.endTs,
+                              size.height,
+                              `rgba(52,69,150,0.3)`);
+      }
     }
   }
 }
