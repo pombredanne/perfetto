f32f782717ca10bd373b5f03403bdb1395e6177a
Oystein Eftevaag <oysteine@google.com>
2019-03-29 09:41:04 -0700

    [Patchset 4] Make sure to always create the SMA on the correct thread in the in-process case
    Requires TracingService::ConnectProducer to be called with in_process=true
    so that the SMA is automatically created when the SMB is created.
    
    R=primiano@google.com
    
    Bug:chromium:935550
    Change-Id: Iaba045c7ad98519e53ebe09bfee99a15df194dd9

--- src/tracing/core/tracing_service_impl.cc
+++ src/tracing/core/tracing_service_impl.cc
@@ -2517,13 +2517,13 @@ TracingServiceImpl::ProducerEndpointImpl::GetShmemArbiter() {
 std::unique_ptr<TraceWriter>
 TracingServiceImpl::ProducerEndpointImpl::CreateTraceWriter(BufferID buf_id) {
   if (!inproc_shmem_arbiter_) {
-    PERFETTO_DCHECK(in_process_);
     PERFETTO_FATAL(
         "ProducerEndpoint::CreateTraceWriter can only be used when "
         "CreateProducer has been called with in_process=true and after tracing "
         "has started.");
   }
 
+  PERFETTO_DCHECK(in_process_);
   return GetShmemArbiter()->CreateTraceWriter(buf_id);
 }
 
